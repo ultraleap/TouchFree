2682d325618de4bb6d19c69e8220a2beb7e0bbca
Matt Gray <matt.gray@ultraleap.com>
2021-01-11 14:30:26 +0000

    Added documentation for ServiceConnection

--- ScreenControl/Assets/ScreenControl/Client/Scripts/CoreConnection/ServiceConnection.cs
+++ ScreenControl/Assets/ScreenControl/Client/Scripts/CoreConnection/ServiceConnection.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
@@ -7,44 +6,71 @@ using WebSocketSharp;
 
 namespace Ultraleap.ScreenControl.Client.Connection
 {
+    // Class: ServiceConnection
+    // This represents a connection to a ScreenControlService. It should be created by a
+    // <ConnectionManager> to ensure there is only one active connection at a time. The sending
+    // and receiving of data to the client is handled here as well as the creation of a
+    // <WebSocketReceiver> to ensure the data is handled properly.
     public class ServiceConnection
     {
+        // Group: Variables
+
+        // Delegate: ClientInputActionEvent
+        // An Action to distribute a <ClientInputAction> via the <TransmitInputAction> event listener.
         public delegate void ClientInputActionEvent(ClientInputAction _inputData);
+        // Variable: TransmitInputAction
+        // An event for transmitting <ClientInputAction> that are received via the <webSocket> to be listened to.
         public event ClientInputActionEvent TransmitInputAction;
 
-        WebSocket ws;
-        WebSocketReceiver receiverQueue;
+        // Variable: webSocket
+        // A reference to the websocket we are connected to.
+        WebSocket webSocket;
 
+        // Variable: receiver
+        // A reference to the receiver that handles ordered queueing of data received via the <webSocket>.
+        WebSocketReceiver receiver;
+
+        // Group: Functions
+
+        // Function: ServiceConnection
+        // The initializer for <ServiceConnection> that can be given a different IP Address and Port to connect to
+        // on initialization. This initializer also sets up the <receiver> for future use and the listener for the 
+        // receiving of messages.
         internal ServiceConnection(string _ip = "127.0.0.1", string _port = "9739")
         {
-            ws = new WebSocket($"ws://{_ip}:{_port}/connect");
+            webSocket = new WebSocket($"ws://{_ip}:{_port}/connect");
             WebSocketSharp.Net.Cookie cookie = new WebSocketSharp.Net.Cookie(VersionInfo.API_HEADER_NAME, VersionInfo.ApiVersion.ToString());
-            ws.SetCookie(cookie);
+            webSocket.SetCookie(cookie);
 
-            ws.OnMessage += (sender, e) =>
+            webSocket.OnMessage += (sender, e) =>
             {
                 OnMessage(e);
             };
 
-            ws.Connect();
+            webSocket.Connect();
 
-            receiverQueue = ConnectionManager.Instance.gameObject.AddComponent<WebSocketReceiver>();
-            receiverQueue.SetWSConnection(this);
+            receiver = ConnectionManager.Instance.gameObject.AddComponent<WebSocketReceiver>();
+            receiver.SetWSConnection(this);
         }
 
+        // Function: Disconnect
+        // Can be used to force the connection to the <webSocket> to be closed. Also destroys the <receiver>.
         public void Disconnect()
         {
-            if (ws != null)
+            if (webSocket != null)
             {
-                ws.Close();
+                webSocket.Close();
             }
 
-            if (receiverQueue != null)
+            if (receiver != null)
             {
-                WebSocketReceiver.Destroy(receiverQueue);
+                WebSocketReceiver.Destroy(receiver);
             }
         }
 
+        // Function: OnMessage
+        // The first point of contact for new messages received, these are sorted into appropriate types based on their
+        // <ActionCodes> and added to queues on the <receiver>.
         public void OnMessage(MessageEventArgs _message)
         {
             string rawData = _message.Data;
@@ -61,7 +87,7 @@ namespace Ultraleap.ScreenControl.Client.Connection
                 case ActionCodes.INPUT_ACTION:
                     WebsocketInputAction wsInput = JsonUtility.FromJson<WebsocketInputAction>(content);
                     ClientInputAction cInput = new ClientInputAction(wsInput);
-                    receiverQueue.actionQueue.Enqueue(cInput);
+                    receiver.actionQueue.Enqueue(cInput);
                     break;
 
                 case ActionCodes.CONFIGURATION_STATE:
@@ -69,16 +95,21 @@ namespace Ultraleap.ScreenControl.Client.Connection
 
                 case ActionCodes.CONFIGURATION_RESPONSE:
                     WebSocketResponse response = JsonUtility.FromJson<WebSocketResponse>(content);
-                    receiverQueue.responseQueue.Enqueue(response);
+                    receiver.responseQueue.Enqueue(response);
                     break;
             }
         }
 
+        // Function: HandleInputAction
+        // Called by the <receiver> to relay a <ClientInputAction> that has ben received to any listeners of <TransmitInputAction>.
         public void HandleInputAction(ClientInputAction _action)
         {
             TransmitInputAction?.Invoke(_action);
         }
 
+        // Function: SendMessage
+        // Used to send or request information from the Service via the <webSocket>. To be given a pre-made <_message> and <_requestID>.
+        // Provides an asynchronous <WebSocketResponse> via the <_callback> parameter.
         internal void SendMessage(string _message, string _requestID, Action<WebSocketResponse> _callback)
         {
             if (_requestID == "")
@@ -95,10 +126,10 @@ namespace Ultraleap.ScreenControl.Client.Connection
 
             if (_callback != null)
             {
-                receiverQueue.responseCallbacks.Add(_requestID, new ResponseCallback(DateTime.Now.Millisecond, _callback));
+                receiver.responseCallbacks.Add(_requestID, new ResponseCallback(DateTime.Now.Millisecond, _callback));
             }
 
-            ws.Send(_message);
+            webSocket.Send(_message);
         }
     }
 }
