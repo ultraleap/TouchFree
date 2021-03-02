c3cfefd55e60f0e7dcb64f5c3ac1406acd4472a4
Nat Greenwood <nat.greenwood@ultrahaptics.com>
2021-03-02 14:55:02 +0000

    First pass, testing on CI system

---
+++ Scripts/build_service.bat
@@ -0,0 +1,1 @@
+%UNITY_EXE% -batchMode -logFile %CI_PROJECT_DIR%\Service_build.log -projectPath %CI_PROJECT_DIR%\ScreenControl_Unity -buildWindows64Player %CI_PROJECT_DIR%\Build\ScreenControlService.exe -quit
