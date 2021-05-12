84eba227bed7296a486cdf54e7d3fa397be17237
Matt Gray <matt.gray@ultraleap.com>
2021-05-12 14:00:54 +0100

    Removed unused 'TouchFreeRingCursor' script. Added 'SetCursorVisibility' to CursorManager & extended CursorManager to listen to 'V' key presses

---
+++ TouchFree/Assets/TouchFree/Scripts/Cursors/TouchFreeCursorManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchFreeCursorManager : CursorManager
+{
+    bool active = true;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            active = !active;
+            SetCursorVisibility(active);
+        }
+    }
+}
