49d39a8233d952a2c7c705b5cdd4da6f34951fff
Matt Gray <matt.gray@ultraleap.com>
2021-05-28 17:03:20 +0100

    Updated naming in /scripts & /documentation

--- Scripts/build_service.bat
+++ Scripts/build_service.bat
@@ -1,1 +1,1 @@
-%1 -batchMode -logFile %CI_PROJECT_DIR%\Service_build.log -projectPath %CI_PROJECT_DIR%\ScreenControl_Unity -buildWindows64Player %CI_PROJECT_DIR%\Build\ScreenControlService.exe -quit
+%1 -batchMode -logFile %CI_PROJECT_DIR%\Service_build.log -projectPath %CI_PROJECT_DIR%\Service_Unity -buildWindows64Player %CI_PROJECT_DIR%\Build\TouchFreeService.exe -quit
