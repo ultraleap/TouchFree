ba3dd6ac56afe1f301b7061878df95b1f9f80f58
Nat Greenwood <nat.greenwood@ultrahaptics.com>
2021-06-01 15:52:10 +0100

    CI Script changes for new folder structure, update some names in Version.txt files`

--- Scripts/build_service.bat
+++ Scripts/build_service.bat
@@ -1,1 +1,1 @@
-%1 -batchMode -logFile %CI_PROJECT_DIR%\Service_build.log -projectPath %CI_PROJECT_DIR%\Service_Unity -buildWindows64Player %CI_PROJECT_DIR%\Build\TouchFreeService.exe -quit
+%1 -batchMode -logFile %CI_PROJECT_DIR%\Service_build.log -projectPath %CI_PROJECT_DIR%\TF_Service_and_Tooling_Unity -buildWindows64Player %CI_PROJECT_DIR%\Build\TouchFreeService.exe -quit
--- Scripts/build_touchfree.bat
+++ Scripts/build_touchfree.bat
@@ -1,1 +1,1 @@
-%1 -batchMode -logFile %CI_PROJECT_DIR%\TouchFree_build.log -projectPath %CI_PROJECT_DIR%\TouchFree -buildWindows64Player %CI_PROJECT_DIR%\TouchFree_Build\TouchFree.exe -quit
+%1 -batchMode -logFile %CI_PROJECT_DIR%\TouchFree_build.log -projectPath %CI_PROJECT_DIR%\TouchFree -buildWindows64Player %CI_PROJECT_DIR%\TouchFree_Build\TF_Application.exe -quit
