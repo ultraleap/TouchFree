a0c52f4fd8a4f04e9b5f766522a8c3079bc7b131
Nat Greenwood <nat.greenwood@ultrahaptics.com>
2021-06-02 11:10:08 +0100

    Correct TF project path in build script`

--- Scripts/build_touchfree.bat
+++ Scripts/build_touchfree.bat
@@ -1,1 +1,1 @@
-%1 -batchMode -logFile %CI_PROJECT_DIR%\TouchFree_build.log -projectPath %CI_PROJECT_DIR%\TouchFree -buildWindows64Player %CI_PROJECT_DIR%\TouchFree_Build\TouchFree_Application.exe -quit
+%1 -batchMode -logFile %CI_PROJECT_DIR%\TouchFree_build.log -projectPath %CI_PROJECT_DIR%\TF_Application -buildWindows64Player %CI_PROJECT_DIR%\TouchFree_Build\TouchFree_Application.exe -quit
