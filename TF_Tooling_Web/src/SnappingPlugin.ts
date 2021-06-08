6462d88e0c01a381a169aeadb7b00584c4c193c3
Rodolphe Houdas <rodolphe.houdas@ultraleap.com>
2021-06-08 17:17:21 +0100

    Clean code and improve logic

--- TF_Tooling_Web/src/SnappingPlugin.ts
+++ TF_Tooling_Web/src/SnappingPlugin.ts
@@ -1,141 +1,57 @@
 import { InputActionPlugin } from "./Plugins/InputActionPlugin";
 import { TouchFreeInputAction } from "./TouchFreeToolingTypes";
 
-export class SnappingPlugin extends InputActionPlugin {
+class SnappableElement {
+    public element: Element;
+    public distance: number;
+    public closest_point: Vector2;
+    public center: Vector2;
+    public center_distance: number;
+    public hovered: boolean
 
-    public _snapDistance: number = 1;
-    public _snapToCenter: boolean = true;
-
-    public static MAX_SOFTNESS: number = 1;
-    public static MIN_SOFTNESS: number = 0;
-
-    public snapSoftness: number = 0.3;
-
-    ModifyInputAction(_inputAction: TouchFreeInputAction): TouchFreeInputAction | null {
-
-        const cursorPos: {x: number, y: number} = {
-            x: _inputAction.CursorPosition[0],
-            y: window.innerHeight - _inputAction.CursorPosition[1]
-        }
-
-        // Build a list of snappable elements and their closest distance from the cursor
-        let elements: {element: Element, distance: number}[] = [...document.getElementsByClassName("snappable")].map(
-            (value: Element, index: number, array: Element[]) => {
-                return {element: value, distance: this.getDistance(value, cursorPos)};
-            }
-        );
-
-        // Sort them by distance
-        elements = elements.sort(
-            (a: {element: Element, distance: number}, b: {element: Element, distance: number}) => {
-            // Negative if a closer than b
-            // 0 if a and b equidistant (should not happen by a slight margin)
-            // Positif if b closer than a
-            return a.distance - b.distance;
-        })
-
-        // Let's snap if there is snappable elements
-        if (elements.length > 0) {
-            let closest_center: {x: number, y: number} = this.getElementCenter(elements[0].element);
-            const closest_distance: number = elements[0].distance;
-
-            if (closest_distance < this._snapDistance) {
-                if (this._snapToCenter) {
-
-                    // If snapForce = 1, cursor position inside the shape is the same
-                    // If snapForce = 0, cursor position is snapped in the middle
-
-                    let snapForce: number = Number.parseFloat(elements[0].element.getAttribute("data-snapforce") ?? this.snapSoftness.toString());
-
-                    const center_distance: number = this.getDistanceBetween(closest_center, cursorPos);
-                    let softSnapT: number = (center_distance / 50) * this.lerp(
-                        SnappingPlugin.MIN_SOFTNESS,
-                        SnappingPlugin.MAX_SOFTNESS,
-                        snapForce);
-
-                    softSnapT = Math.max(Math.min(softSnapT, 1), 0);
-
-                    const finalPos: {x: number, y: number} = {x: 0, y: 0};
-                    finalPos.x = this.lerp(closest_center.x, cursorPos.x, softSnapT);
-                    finalPos.y = this.lerp(closest_center.y, cursorPos.y, softSnapT);
-
-                    _inputAction.CursorPosition[0] = finalPos.x;
-                    _inputAction.CursorPosition[1] = (window.innerHeight - finalPos.y);
-                }
-                else {
-                    if (!this.hasSnappableUnder([cursorPos.x, cursorPos.y])) {
-                        closest_center = this.raycast(cursorPos, this.getElementCenter(elements[0].element), elements[0].element);
-
-                        _inputAction.CursorPosition[0] = closest_center.x;
-                        _inputAction.CursorPosition[1] = (window.innerHeight - closest_center.y);
-                    }
-                }
-            }
-        }
-
-        return _inputAction;
+    constructor(
+            element: Element,
+            distance: number,
+            closest_point: Vector2,
+            center: Vector2,
+            center_distance: number,
+            hovered: boolean
+    ) {
+        this.element = element;
+        this.distance = distance;
+        this.closest_point = closest_point;
+        this.center = center;
+        this.center_distance = center_distance;
+        this.hovered = hovered;
     }
 
-    private getDistance(element: Element, cursorPos: {x: number, y: number}): number {
-        let center : {x: number, y: number} = this.getElementCenter(element);
-
-        center = this.raycast({x: cursorPos.x, y: cursorPos.y}, center, element);
-
-        return Math.sqrt(
-            Math.pow(cursorPos.x - center.x, 2) +
-            Math.pow(cursorPos.y - center.y, 2)
-        );
-    }
-
-    private getDistanceBetween(elementPos: {x: number, y: number}, cursorPos: {x: number, y: number}): number {
-        return Math.sqrt(
-            Math.pow(cursorPos.x - elementPos.x, 2) +
-            Math.pow(cursorPos.y - elementPos.y, 2)
-        );
-    }
-
-    private getElementCenter(element: Element): {x: number, y: number} {
+    public static Compute(element: Element, distant_point: Vector2): SnappableElement {
         const rect : DOMRect = element.getBoundingClientRect();
-        return {
-            x: rect.x + (rect.width / 2),
-            y: rect.y + (rect.height / 2)
-        };
+        const center: Vector2 = new Vector2(rect.x + (rect.width / 2), rect.y + (rect.height / 2));
+        const center_distance: number = Math.sqrt(
+            Math.pow(distant_point.x - center.x, 2) +
+            Math.pow(distant_point.y - center.y, 2)
+        );
+
+        const closest_point: Vector2 = Ray.Cast(distant_point, center, element);
+        const distance: number = Math.sqrt(
+            Math.pow(distant_point.x - closest_point.x, 2) +
+            Math.pow(distant_point.y - closest_point.y, 2)
+        );
+
+        const hovered: boolean = closest_point === distant_point;
+
+        return new SnappableElement(element, distance, closest_point, center, center_distance, hovered);
     }
+}
 
-    // Search for an element with "snappable" class under the cursor
-    private getTopSnappableElement(_position: Array<number>): Element | null {
-        let elementsAtPos: Element[] | null = document.elementsFromPoint(
-            _position[0],
-            _position[1]);
+class Ray {
 
-        let elementAtPos: Element | undefined = undefined;
+    public static Cast(start: Vector2, end: Vector2, element: Element): Vector2 {
 
-        if (elementsAtPos !== null) {
-            elementAtPos = elementsAtPos.find((value: Element, index: number, obj: Element[]) => value.classList.contains("snappable"));
-        }
+        const pos: Vector2 = new Vector2(start.x, start.y);
 
-        return elementAtPos === undefined ? null : elementAtPos;
-    }
-
-    private hasSnappableUnder(_position: Array<number>): boolean {
-        return this.getTopSnappableElement(_position) !== null;
-    }
-
-    private hasSnappableUnderIs(_position: Array<number>, element: Element): boolean {
-        return this.getTopSnappableElement(_position) === element;
-    }
-
-    private raycast(
-            start: {x: number, y: number},
-            end: {x: number, y: number},
-            element: Element
-    ): {x: number, y: number} {
-        const pos: {x: number, y: number} = {
-            x: start.x,
-            y: start.y
-        }
-
-        let hasSnap: boolean = this.hasSnappableUnderIs([pos.x, pos.y], element);
+        let hasSnap: boolean = this.hit(pos, element);
         let hadSnap: boolean = hasSnap;
 
         // If we already have snappable under us, it means we already are in a button
@@ -169,7 +85,7 @@ export class SnappingPlugin extends InputActionPlugin {
         pos.y = pos.y + (vector.y / 2);
 
         while (distance > 1) {
-            hasSnap = this.hasSnappableUnderIs([pos.x, pos.y], element);
+            hasSnap = this.hit(pos, element);
 
             // If we changed state, we reverse the direction
             if ((hasSnap && !hadSnap) || (!hasSnap && hadSnap)) {
@@ -196,6 +112,91 @@ export class SnappingPlugin extends InputActionPlugin {
         }
 
         return pos;
+    }
+
+    private static hit(position: Vector2, element: Element): boolean {
+        let elementsAtPos: Element[] = document.elementsFromPoint(position.x, position.y);
+        return elementsAtPos.find((value: Element, index: number, obj: Element[]) => value === element) !== undefined;
+    }
+}
+
+class Vector2 {
+    public x: number;
+    public y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static FromTuple(tuple: {x: number, y: number}): Vector2 {
+        return new Vector2(tuple.x, tuple.y);
+    }
+
+    public toTuple(): {x: number, y: number} {
+        return {x: this.x, y: this.y};
+    }
+}
+
+export class SnappingPlugin extends InputActionPlugin {
+
+    public _snapDistance: number = 25;
+    public _snapToCenter: boolean = false;
+
+    public static MAX_SOFTNESS: number = 1;
+    public static MIN_SOFTNESS: number = 0;
+
+    public snapSoftness: number = 0.3;
+
+    ModifyInputAction(_inputAction: TouchFreeInputAction): TouchFreeInputAction | null {
+
+        const cursorPos: {x: number, y: number} = {
+            x: _inputAction.CursorPosition[0],
+            y: window.innerHeight - _inputAction.CursorPosition[1]
+        }
+
+        // Build a list of snappable elements
+        let elements: SnappableElement[] = [...document.getElementsByClassName("snappable")].map(
+            (value: Element, index: number, array: Element[]) => {
+                return SnappableElement.Compute(value, Vector2.FromTuple(cursorPos));
+            }
+        );
+
+        // Sort them by distance
+        elements = elements.sort((a: SnappableElement, b: SnappableElement) => {return a.distance - b.distance;})
+
+        // Let's snap if there is snappable elements
+        if (elements.length > 0) {
+            if (elements[0].distance < this._snapDistance) {
+                if (this._snapToCenter) {
+                    // If snapForce = 1, cursor position inside the shape is the same
+                    // If snapForce = 0, cursor position is snapped in the middle
+                    let snapForce: number = Number.parseFloat(elements[0].element.getAttribute("data-snapforce") ?? this.snapSoftness.toString());
+
+                    let softSnapT: number = (elements[0].center_distance / 50) * this.lerp(
+                        SnappingPlugin.MIN_SOFTNESS,
+                        SnappingPlugin.MAX_SOFTNESS,
+                        snapForce);
+
+                    softSnapT = Math.max(Math.min(softSnapT, 1), 0);
+
+                    const finalPos: {x: number, y: number} = {x: 0, y: 0};
+                    finalPos.x = this.lerp(elements[0].center.x, cursorPos.x, softSnapT);
+                    finalPos.y = this.lerp(elements[0].center.y, cursorPos.y, softSnapT);
+
+                    _inputAction.CursorPosition[0] = finalPos.x;
+                    _inputAction.CursorPosition[1] = (window.innerHeight - finalPos.y);
+                }
+                else {
+                    if (!elements[0].hovered) {
+                        _inputAction.CursorPosition[0] = elements[0].closest_point.x;
+                        _inputAction.CursorPosition[1] = (window.innerHeight - elements[0].closest_point.y);
+                    }
+                }
+            }
+        }
+
+        return _inputAction;
     }
 
     private lerp(x: number, y: number, a: number): number {
