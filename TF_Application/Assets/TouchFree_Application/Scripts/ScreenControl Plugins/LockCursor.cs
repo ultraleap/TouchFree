3b5aedd803a60c83a0076629e6a6349de044e74d
Matt Gray <matt.gray@ultraleap.com>
2021-08-02 15:45:17 +0100

    Added a scaling remap to TouchFreeMain to adjust the cursor window size based on the cursor size.
    - Window sizes allow for max thickness rings at all times

--- TF_Application/Assets/TouchFree_Application/Scripts/ScreenControl Plugins/LockCursor.cs
+++ TF_Application/Assets/TouchFree_Application/Scripts/ScreenControl Plugins/LockCursor.cs
@@ -8,19 +8,13 @@ namespace Ultraleap.TouchFree
     public class LockCursor : InputActionPlugin
     {
         public TransparentWindow window;
-        private Vector2 screenMiddle;
-
-        void Start()
-        {
-            screenMiddle = new Vector2(TouchFreeMain.CursorWindowSize / 2, TouchFreeMain.CursorWindowSize / 2);
-        }
 
         protected override InputAction? ModifyInputAction(InputAction _inputAction)
         {
             if (window.clickThroughEnabled)
             {
                 window.SetPosition(new Vector2(_inputAction.CursorPosition.x, _inputAction.CursorPosition.y));
-                _inputAction.CursorPosition = screenMiddle;
+                _inputAction.CursorPosition = TouchFreeMain.CursorWindowMiddle;
             }
 
             return _inputAction;
--- TF_Application/Assets/TouchFree_Application/Scripts/TouchFreeMain.cs
+++ TF_Application/Assets/TouchFree_Application/Scripts/TouchFreeMain.cs
@@ -5,11 +5,48 @@ namespace Ultraleap.TouchFree
     [DefaultExecutionOrder(-1)]
     public class TouchFreeMain : MonoBehaviour
     {
-        public static int CursorWindowSize = 256;
+        public static int CursorWindowSize = 200;
+        public static Vector3 CursorWindowMiddle = new Vector2(100, 100);
+
+        static int minCursorWindowSize = 50;
+        static int maxCursorWindowSize = 500;
 
         void Awake()
         {
             Application.targetFrameRate = 60;
+        }
+
+        void OnEnable()
+        {
+            ConfigManager.Config.OnConfigUpdated += ConfigUpdated;
+            ConfigUpdated();
+        }
+
+        void OnDisable()
+        {
+            ConfigManager.Config.OnConfigUpdated -= ConfigUpdated;
+        }
+
+        void ConfigUpdated()
+        {
+            if(ConfigManager.Config.cursorEnabled)
+            {
+                UpdateCursorWindowSize(ConfigManager.Config.cursorSizeCm);
+            }
+            else
+            {
+                // If we do not need to render a cursor, make it very small so reduce GPU load
+                UpdateCursorWindowSize(2);
+            }
+        }
+
+        public static void UpdateCursorWindowSize(float _cursorSize)
+        {
+            int newWindowSize = (int)Tooling.Utilities.MapRangeToRange(
+                _cursorSize, 0.1f, 1f, minCursorWindowSize, maxCursorWindowSize);
+
+            CursorWindowSize = newWindowSize;
+            CursorWindowMiddle = Vector2.one * (CursorWindowSize / 2);
         }
     }
 }
