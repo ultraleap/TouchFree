365b3b123fa2b62e72be3596d7438f320fecb6e8
Matt Gray <matt.gray@ultraleap.com>
2021-08-18 12:33:39 +0100

    TouchFree Service will create an empty TouchFreeApp config file if one does not already exist

--- TF_Service_and_Tooling_Unity/Assets/TouchFree/Service/Scripts/ServiceConfigHandler.cs
+++ TF_Service_and_Tooling_Unity/Assets/TouchFree/Service/Scripts/ServiceConfigHandler.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
+using System.IO;
 using Ultraleap.TouchFree.ServiceShared;
 
 namespace Ultraleap.TouchFree.Service
 {
     public class ServiceConfigHandler : MonoBehaviour
     {
+        const string TOUCHFREE_APP_CONFIG_NAME = "TouchFreeConfig.json";
+
         void Start()
         {
             ClientConnectionManager.Instance.LostAllConnections += OnLostAllConnections;
+
+            HandleTFAppConfigSetup();
         }
 
         private void OnDestroy()
@@ -20,6 +25,21 @@ namespace Ultraleap.TouchFree.Service
             ConfigManager.LoadConfigsFromFiles();
             ConfigManager.InteractionConfig.ConfigWasUpdated();
             ConfigManager.PhysicalConfig.ConfigWasUpdated();
+        }
+
+        void HandleTFAppConfigSetup()
+        {
+            if(!Directory.Exists(ConfigFileUtils.ConfigFileDirectory))
+            {
+                Directory.CreateDirectory(ConfigFileUtils.ConfigFileDirectory);
+            }
+
+            string filePath = Path.Combine(ConfigFileUtils.ConfigFileDirectory, TOUCHFREE_APP_CONFIG_NAME);
+
+            if(!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, "{}");
+            }
         }
     }
 }
