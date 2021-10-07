c5788dd74a07e61f4727e185eeed834cf7190048
Nat Greenwood <nat.greenwood@ultrahaptics.com>
2021-10-07 15:42:59 +0100

    New Camera Previews screen with ability to highlight bright spots

---
+++ TF_Service_and_Tooling_Unity/Assets/TouchFree/ServiceUI/Scripts/Configuration/Screens/CameraPreviewScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraPreviewScreen : MonoBehaviour
+{
+    public Toggle enableOverexposureHighlighting;
+    public Material leftCameraMat;
+    public Material rightCameraMat;
+
+    void OnEnable()
+    {
+        enableOverexposureHighlighting.onValueChanged.AddListener(OnOverExposureValueChanged);
+        OnOverExposureValueChanged(enableOverexposureHighlighting.isOn);
+    }
+
+    void OnDisable()
+    {
+        enableOverexposureHighlighting.onValueChanged.RemoveListener(OnOverExposureValueChanged);
+    }
+
+    void OnOverExposureValueChanged(bool state)
+    {
+        if (state) {
+            leftCameraMat.SetFloat("_threshold", 0.5f);
+            rightCameraMat.SetFloat("_threshold", 0.5f);
+        } else {
+            leftCameraMat.SetFloat("_threshold", 1.0f);
+            rightCameraMat.SetFloat("_threshold", 1.0f);
+        }
+    }
+}
