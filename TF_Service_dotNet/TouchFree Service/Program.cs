5184129f0d057fffba7e91b6f74d34ac57f69651
Peter Nancollis <peter.nancollis@ultrahaptics.com>
2021-10-15 14:25:04 +0100

    Rename to TouchFreeLib and add update behaviour with timer

--- TF_Service_dotNet/TouchFree Service/Program.cs
+++ TF_Service_dotNet/TouchFree Service/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Timers;
 
 namespace Ultraleap.TouchFree.Service
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            UpdateBehaviour updateLoop = new();
+            updateLoop.OnUpdate += TickTock;
+
             Console.WriteLine("TouchFree physical config screen height is: " + Configuration.ConfigManager.PhysicalConfig.ScreenHeightM);
+            while(true)
+            {
+
+            }
+        }
+
+        private static void TickTock()
+        {
+            Console.WriteLine("TickTock");
         }
     }
 }
