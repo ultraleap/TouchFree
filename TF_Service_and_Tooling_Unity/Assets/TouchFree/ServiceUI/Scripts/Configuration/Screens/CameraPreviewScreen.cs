5f6fd294660e34886d3d2c8141a9da2d6a05e52c
MattGray <matt.gray@ultraleap.com>
2021-10-30 09:56:14 +0100

    Added references to the DiagnosticAPI to set the masking via sliders

--- TF_Service_and_Tooling_Unity/Assets/TouchFree/ServiceUI/Scripts/Configuration/Screens/CameraPreviewScreen.cs
+++ TF_Service_and_Tooling_Unity/Assets/TouchFree/ServiceUI/Scripts/Configuration/Screens/CameraPreviewScreen.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,27 +9,105 @@ public class CameraPreviewScreen : MonoBehaviour
     public Material rightCameraMat;
 
     [SerializeField]
-    private float thresholdValue = 0.5f;
+    private float exposureThresholdValue = 0.5f;
+
+    DiagnosticAPI diagnosticAPI;
+
+    public Slider maskingSiderL, maskingSiderR, maskingSiderT, maskingSiderB;
+
+    [Tooltip("The ratio of the slider length to the masking capacity. Total capacity is the full width/height of the camera image.")]
+    public float sliderRatio = 0.5f;
+
+    DiagnosticAPI.ImageMaskData currentMaskData;
+    bool newMaskDataReceived = false;
 
     void OnEnable()
     {
         enableOverexposureHighlighting.onValueChanged.AddListener(OnOverExposureValueChanged);
         OnOverExposureValueChanged(enableOverexposureHighlighting.isOn);
+
+        if(diagnosticAPI == null)
+        {
+            diagnosticAPI = new DiagnosticAPI();
+        }
+
+        DiagnosticAPI.OnGetMaskingResponse += ReceiveMaskValues;
+
+        maskingSiderL.onValueChanged.AddListener(OnSliderChanged);
+        maskingSiderR.onValueChanged.AddListener(OnSliderChanged);
+        maskingSiderT.onValueChanged.AddListener(OnSliderChanged);
+        maskingSiderB.onValueChanged.AddListener(OnSliderChanged);
+
+        diagnosticAPI.Request("GetImageMask:1");
     }
 
     void OnDisable()
     {
         enableOverexposureHighlighting.onValueChanged.RemoveListener(OnOverExposureValueChanged);
+        DiagnosticAPI.OnGetMaskingResponse -= ReceiveMaskValues;
+
+        maskingSiderL.onValueChanged.RemoveListener(OnSliderChanged);
+        maskingSiderR.onValueChanged.RemoveListener(OnSliderChanged);
+        maskingSiderT.onValueChanged.RemoveListener(OnSliderChanged);
+        maskingSiderB.onValueChanged.RemoveListener(OnSliderChanged);
+    }
+
+    private void Update()
+    {
+        if(newMaskDataReceived)
+        {
+            maskingSiderL.SetValueWithoutNotify((float)currentMaskData.left / sliderRatio);
+            maskingSiderR.SetValueWithoutNotify((float)currentMaskData.right / sliderRatio);
+            maskingSiderT.SetValueWithoutNotify((float)currentMaskData.lower / sliderRatio);// These are reversed as the shader for rendering camera feeds is upside-down
+            maskingSiderB.SetValueWithoutNotify((float)currentMaskData.upper / sliderRatio);// These are reversed as the shader for rendering camera feeds is upside-down
+            newMaskDataReceived = false;
+        }
     }
 
     void OnOverExposureValueChanged(bool state)
     {
         if (state) {
-            leftCameraMat.SetFloat("_threshold", thresholdValue);
-            rightCameraMat.SetFloat("_threshold", thresholdValue);
+            leftCameraMat.SetFloat("_threshold", exposureThresholdValue);
+            rightCameraMat.SetFloat("_threshold", exposureThresholdValue);
         } else {
             leftCameraMat.SetFloat("_threshold", 1.0f);
             rightCameraMat.SetFloat("_threshold", 1.0f);
         }
+    }
+
+    void SetMasking()
+    {
+        currentMaskData.device_id = 1; // not used but must not be 0
+        currentMaskData.left = maskingSiderL.value * sliderRatio;
+        currentMaskData.right = maskingSiderR.value * sliderRatio;
+        currentMaskData.upper = maskingSiderB.value * sliderRatio; // These are reversed as the shader for rendering camera feeds is upside-down
+        currentMaskData.lower = maskingSiderT.value * sliderRatio; // These are reversed as the shader for rendering camera feeds is upside-down
+
+        diagnosticAPI.Request("SetImageMask:" + JsonUtility.ToJson(currentMaskData));
+    }
+
+    void SetSliders(DiagnosticAPI.ImageMaskData _maskValues)
+    {
+        currentMaskData = _maskValues;
+        maskingSiderL.SetValueWithoutNotify((float)currentMaskData.left / sliderRatio);
+        maskingSiderR.SetValueWithoutNotify((float)currentMaskData.right / sliderRatio);
+        maskingSiderT.SetValueWithoutNotify((float)currentMaskData.lower / sliderRatio);// These are reversed as the shader for rendering camera feeds is upside-down
+        maskingSiderB.SetValueWithoutNotify((float)currentMaskData.upper / sliderRatio);// These are reversed as the shader for rendering camera feeds is upside-down
+    }
+
+    public void ReceiveMaskValues(DiagnosticAPI.ImageMaskData _maskValues)
+    {
+        // This method is called on a thread via WebSocketSharp so Invoke a Delegate to ensure it is performed on the main thread
+        var updateValuesDelegate = new Action(delegate ()
+        {
+            SetSliders(_maskValues);
+        });
+
+        updateValuesDelegate.Invoke();
+    }
+
+    public void OnSliderChanged(float _)
+    {
+        SetMasking();
     }
 }
---
+++ TF_Service_and_Tooling_Unity/Assets/TouchFree/ServiceUI/Scripts/DiagnosticAPI.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using WebSocketSharp;
+
+public class DiagnosticAPI : IDisposable
+{
+    private static string uri = "ws://127.0.0.1:1024/";
+
+    public enum Status { Closed, Connecting, Connected, Expired }
+    private Status status = Status.Expired;
+    private WebSocket webSocket = null;
+
+    public delegate void GetMaskingResponseEvent(ImageMaskData _data);
+    public static event GetMaskingResponseEvent OnGetMaskingResponse;
+
+    public DiagnosticAPI()
+    {
+        Debug.Log("DiagnosticAPI constructor... ");
+        Connect();
+    }
+
+    private void Connect()
+    {
+        if (status == Status.Connecting || status == Status.Connected)
+        {
+            return;
+        }
+
+        bool requireSetup = status == Status.Expired;
+        status = Status.Connecting;
+
+        if (requireSetup)
+        {
+            Debug.Log("DiagnosticAPI setup.");
+
+            if (webSocket != null)
+            {
+                Debug.Log("DiagnosticAPI close previous.");
+                webSocket.Close();
+            }
+
+            webSocket = new WebSocket(uri);
+            webSocket.OnMessage += onMessage;
+            webSocket.OnOpen += (sender, e) => {
+                Debug.Log("DiagnosticAPI open... ");
+                status = Status.Connected;
+            };
+            webSocket.OnError += (sender, e) => {
+                Debug.Log("DiagnosticAPI error! " + e.Message + "\n" + e.Exception.ToString() );
+                status = Status.Expired;
+            };
+            webSocket.OnClose += (sender, e) => {
+                Debug.Log("DiagnosticAPI closed. " + e.Reason);
+                status = Status.Closed;
+            };
+        }
+
+        Debug.Log("DiagnosticAPI connecting... ");
+        try
+        {
+            webSocket.Connect();
+        }
+        catch (System.Exception ex)
+        { 
+            Debug.Log("DiagnosticAPI connection exception... " + "\n" + ex.ToString() );
+            status = Status.Expired;
+        }
+    }
+
+    private void onMessage (object sender, MessageEventArgs e)
+    {
+        if (e.IsText)
+        {
+            try
+            {
+                GetImageMaskResponse maskResponse = JsonUtility.FromJson<GetImageMaskResponse>(e.Data);
+
+                if (maskResponse.request.Contains("GetImageMask"))
+                {
+                    OnGetMaskingResponse?.Invoke(maskResponse.value);
+                }
+            }
+            catch
+            {
+                Debug.Log("Received a non-masking message from the DiagnosticAPI");
+            }
+        }
+    }
+
+    public void Request(string key)
+    {
+        if (status == Status.Connected)
+        {
+            webSocket.Send(key);
+        }
+        else
+        {
+            Connect();
+        }
+    }
+
+    void IDisposable.Dispose ()
+    {
+        status = Status.Expired;
+        webSocket.Close();
+    }
+
+
+    [Serializable]
+    public struct ImageMaskData
+    {
+        public uint device_id;
+        public double lower;
+        public double upper;
+        public double left;
+        public double right;
+    }
+
+    public struct GetImageMaskResponse
+    {
+        public string request;
+        public int status;
+        public ImageMaskData value;
+    }
+}
