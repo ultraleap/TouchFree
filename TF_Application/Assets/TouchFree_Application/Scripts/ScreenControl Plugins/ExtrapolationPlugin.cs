c532012c32d7acdb569c3dc203ec1a5c7095e24b
MattGray <matt.gray@ultraleap.com>
2021-12-10 09:57:07 +0000

    adjusted system cursor layout, added extrapolation plugin

---
+++ TF_Application/Assets/TouchFree_Application/Scripts/ScreenControl Plugins/ExtrapolationPlugin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ultraleap.TouchFree.Tooling;
+
+public class ExtrapolationPlugin : InputActionPlugin
+{
+    InputAction lastAction;
+
+    [Range(0, 10)]
+    public float extrapolationAmount = 2;
+    float actionTime;
+
+    protected override InputAction? ModifyInputAction(InputAction action)
+    {
+        InputAction newAction = action;
+        actionTime = ((action.Timestamp - lastAction.Timestamp) / 1000000f) / Time.deltaTime;
+
+        newAction.CursorPosition = Vector2.Lerp(newAction.CursorPosition, newAction.CursorPosition + (newAction.CursorPosition - lastAction.CursorPosition) * extrapolationAmount / actionTime, 0.2f);
+
+        lastAction = action;
+
+        return newAction;
+    }
+}
