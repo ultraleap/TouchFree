61eeadb8e7c642dfd780579d048d5cb41862b158
MattGray <matt.gray@ultraleap.com>
2022-01-11 13:16:04 +0000

    replaced touch input with mouse input

---
+++ TF_Application/Assets/TouchFree_Application/Scripts/Input/MouseController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    [DllImport("user32.dll")]
+    static extern void mouse_event(int dwFlags, int dx, int dy,
+                          int dwData, int dwExtraInfo);
+
+    //[DllImport("user32.dll")]
+    //static extern IntPtr CreateCursor(IntPtr hInst, int xHotSpot, int yHotSpot,
+    //                                        int nWidth, int nHeight, byte[] pvANDPlane, byte[] pvXORPlane);
+
+    //[DllImport("user32.dll")]
+    //static extern IntPtr LoadCursor(IntPtr hInstance, int lpCursorName);
+
+    //[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    //static extern IntPtr LoadImage(IntPtr hinst, string lpszName, uint uType,
+    //                                    int cxDesired, int cyDesired, uint fuLoad);
+
+    //[DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    //private static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hFile, LoadLibraryFlags dwFlags);
+
+    //[DllImport("user32.dll")]
+    //public static extern IntPtr SetCursor(IntPtr? handle);
+
+    //[DllImport("user32.dll")]
+    //static extern int ShowCursor(bool bShow);
+
+    //private enum LoadLibraryFlags : uint
+    //{
+    //    DONT_RESOLVE_DLL_REFERENCES = 0x00000001,
+    //    LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010,
+    //    LOAD_LIBRARY_AS_DATAFILE = 0x00000002,
+    //    LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040,
+    //    LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020,
+    //    LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008
+    //}
+
+    [Flags]
+    public enum MouseEventFlags
+    {
+        LEFTDOWN = 0x00000002,
+        LEFTUP = 0x00000004,
+        MIDDLEDOWN = 0x00000020,
+        MIDDLEUP = 0x00000040,
+        MOVE = 0x00000001,
+        ABSOLUTE = 0x00008000,
+        RIGHTDOWN = 0x00000008,
+        RIGHTUP = 0x00000010
+    }
+
+
+    public static void SendEvent(MouseEventFlags _event, int x, int y)
+    {
+        x = (int)Ultraleap.TouchFree.Tooling.Utilities.MapRangeToRange(x, 0, Display.main.systemWidth, 0, 65535);
+        y = (int)Ultraleap.TouchFree.Tooling.Utilities.MapRangeToRange(y, 0, Display.main.systemHeight, 0, 65535);
+
+        mouse_event((int)(_event | MouseEventFlags.ABSOLUTE), x, y, 0, 0);
+    }
+}
--- TF_Application/Assets/TouchFree_Application/Scripts/Input/WindowsInputController.cs
+++ TF_Application/Assets/TouchFree_Application/Scripts/Input/WindowsInputController.cs
@@ -12,7 +12,7 @@ public class WindowsInputController : InputController
     protected override void Start()
     {
         DelayedDown.InputOverrideInputAction += HandleInputAction;
-        TouchInjection.Initialize(10, TouchFeedback.NONE);
+        //TouchInjection.Initialize(10, TouchFeedback.NONE);
 
         touches = new PointerTouchInfo[1];
         touches[0].PointerInfo.PointerInputType = PointerInputType.TOUCH;
@@ -43,7 +43,8 @@ public class WindowsInputController : InputController
                 touches[0].PointerInfo.PointerFlags = PointerFlags.DOWN | PointerFlags.INCONTACT | PointerFlags.INRANGE;
                 touches[0].PointerInfo.PtPixelLocation.X = x;
                 touches[0].PointerInfo.PtPixelLocation.Y = y;
-                TouchInjection.Send(touches);
+                //TouchInjection.Send(touches);
+                MouseController.SendEvent(MouseController.MouseEventFlags.LEFTDOWN, x, y);
                 pressing = true;
                 break;
             case InputType.MOVE:
@@ -57,21 +58,31 @@ public class WindowsInputController : InputController
                 }
                 touches[0].PointerInfo.PtPixelLocation.X = x;
                 touches[0].PointerInfo.PtPixelLocation.Y = y;
-                TouchInjection.Send(touches);
+                //TouchInjection.Send(touches);
+                MouseController.SendEvent(MouseController.MouseEventFlags.MOVE, x, y);
+
                 break;
             case InputType.UP:
                 touches[0].PointerInfo.PointerFlags = PointerFlags.UP | PointerFlags.INRANGE;
                 touches[0].PointerInfo.PtPixelLocation.X = x;
                 touches[0].PointerInfo.PtPixelLocation.Y = y;
-                TouchInjection.Send(touches);
+                //TouchInjection.Send(touches);
+                MouseController.SendEvent(MouseController.MouseEventFlags.LEFTUP, x, y);
+
                 pressing = false;
                 break;
             case InputType.CANCEL:
                 touches[0].PointerInfo.PointerFlags = PointerFlags.CANCELLED | PointerFlags.UP;
                 touches[0].PointerInfo.PtPixelLocation.X = x;
                 touches[0].PointerInfo.PtPixelLocation.Y = y;
-                TouchInjection.Send(touches);
+                //TouchInjection.Send(touches);
+                MouseController.SendEvent(MouseController.MouseEventFlags.MOVE, x, y);
+
                 pressing = false;
+                break;
+            default:
+                MouseController.SendEvent(MouseController.MouseEventFlags.MOVE, x, y);
+
                 break;
         }
     }
