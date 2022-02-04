4dd396f183f115666f67b9e06a5c4296ce1a97fc
MattGray <matt.gray@ultraleap.com>
2022-02-04 16:34:46 +0000

    test commit

--- TF_Service_and_Tooling_Unity/Assets/TouchFree/Service/Scripts/Interactions/InteractionManager.cs
+++ TF_Service_and_Tooling_Unity/Assets/TouchFree/Service/Scripts/Interactions/InteractionManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+
 using Ultraleap.TouchFree.ServiceShared;
 
 namespace Ultraleap.TouchFree.Service
