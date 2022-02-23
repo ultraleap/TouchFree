d61d0b388a70aec41f0201aad51ff0baffb8113f
Nat Greenwood <nat.greenwood@ultrahaptics.com>
2022-02-23 10:50:01 +0000

    Base React Typescript app setup from create-react-app

---
+++ TF_Settings_Web/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Load tooling here & use throughout
+
+// <ControlBar/>
+// <ScreenManager>
+    // Literally just put a blue box here called ManualSetup
+    // Literally just put a red box here called InteractionSetup (background?)
+// </ScreenManager>
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
