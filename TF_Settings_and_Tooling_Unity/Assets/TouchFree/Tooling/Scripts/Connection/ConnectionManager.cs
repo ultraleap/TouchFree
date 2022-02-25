19e66de631f2a6ebdf3782bffa14f238d2935ffe
Gabe Homewood <gabriel.homewood@ultraleap.com>
2022-02-25 14:25:15 +0000

    Fix function naming issue

--- TF_Settings_and_Tooling_Unity/Assets/TouchFree/Tooling/Scripts/Connection/ConnectionManager.cs
+++ TF_Settings_and_Tooling_Unity/Assets/TouchFree/Tooling/Scripts/Connection/ConnectionManager.cs
@@ -147,7 +147,7 @@ namespace Ultraleap.TouchFree.Tooling.Connection
         // Function: RequestServiceStatus
         // Used to request a <ServiceStatus> from the Service via the <webSocket>.
         // Provides an asynchronous <ServiceStatus> via the _callback parameter.
-        public static void RequestConfigState(Action<ServiceStatus> _callback)
+        public static void RequestServiceStatus(Action<ServiceStatus> _callback)
         {
             ConnectionManager.serviceConnection.RequestServiceStatus(_callback);
         }
