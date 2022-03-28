ea811192d421fd70c86929e69c2a32da6f9d2dc9
Nat Greenwood <nat.greenwood@ultrahaptics.com>
2022-03-28 14:58:33 +0100

    Add missing file from last commit

---
+++ TF_Settings_Web/src/Components/SettingsTypes.ts
@@ -0,0 +1,30 @@
+import { HoverAndHoldInteractionSettings, InteractionConfigFull, TouchPlaneInteractionSettings, TrackedPosition } from "../TouchFree/Configuration/ConfigurationTypes";
+import { InteractionType } from "../TouchFree/TouchFreeToolingTypes";
+
+const DefaultHoverAndHoldInteractionSettings: HoverAndHoldInteractionSettings =
+{
+    HoverStartTimeS: 0.5,
+    HoverCompleteTimeS: 0.6,
+};
+
+const DefaultTouchPlaneInteractionSettings: TouchPlaneInteractionSettings =
+{
+    TouchPlaneActivationDistanceCM: 5,
+    TouchPlaneTrackedPosition: TrackedPosition.NEAREST,
+};
+
+export const DefaultInteractionConfig: InteractionConfigFull =
+{
+    UseScrollingOrDragging: false,
+    DeadzoneRadius: 0.003,
+
+    InteractionZoneEnabled: false,
+    InteractionMinDistanceCm: 0.0,
+    InteractionMaxDistanceCm: 25.0,
+
+    InteractionType: InteractionType.PUSH,
+
+    // Interaction-specific settings
+    HoverAndHold: DefaultHoverAndHoldInteractionSettings,
+    TouchPlane: DefaultTouchPlaneInteractionSettings,
+};
