f7919fe16cdbd3b3ddca3c8acdbffeeb2819ba0e
MattGray <matt.gray@ultraleap.com>
2022-04-08 15:09:03 +0100

    adjust timers so CTI stays running for 20s when a new users hand enters

--- TF_Application/Assets/MiniCTI.cs
+++ TF_Application/Assets/MiniCTI.cs
@@ -3,31 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using Ultraleap.TouchFree.Tooling;
 using UnityEngine.Video;
+using System.Diagnostics;
 
 public class MiniCTI : MonoBehaviour
 {
     public GameObject miniCTI;
     public VideoPlayer player;
 
+    public float newUserTimeS = 10;
+    public float forceVideoTimeS = 20;
+
+    bool forceVideo = true;
+
+    Stopwatch newUserStopwatch = new Stopwatch();
+    Stopwatch forceVideoStopwatch = new Stopwatch();
+
     private void Awake()
     {
         InputActionManager.TransmitInputAction += HandleInputAction;
-        miniCTI.SetActive(false);
 
         currentCTIWait = StartCoroutine(ShowMiniCTIAfterWait());
 
         player.url = Application.streamingAssetsPath + "/Mini CTI.mp4";
+
+        Ultraleap.TouchFree.Tooling.Connection.ConnectionManager.HandFound += HandFound;
+        Ultraleap.TouchFree.Tooling.Connection.ConnectionManager.HandsLost += HandLost;
+
+        forceVideoStopwatch.Start();
     }
 
     private void HandleInputAction(InputAction _inputData)
     {
         if(_inputData.InputType == InputType.DOWN)
         {
-            miniCTI.SetActive(false);
-
-            if (currentCTIWait != null)
+            if (!forceVideo || (forceVideo && forceVideoStopwatch.ElapsedMilliseconds > forceVideoTimeS * 1000))
             {
-                StopCoroutine(currentCTIWait);
+                forceVideo = false;
+                miniCTI.SetActive(false);
+
+                if (currentCTIWait != null)
+                {
+                    StopCoroutine(currentCTIWait);
+                    currentCTIWait = null;
+                }
             }
         }
 
@@ -46,9 +64,33 @@ public class MiniCTI : MonoBehaviour
 
     IEnumerator ShowMiniCTIAfterWait()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(4);
 
         miniCTI.SetActive(true);
         currentCTIWait = null;
+    }
+
+    private void HandLost()
+    {
+        newUserStopwatch.Restart();
+    }
+
+    private void HandFound()
+    {
+        if(newUserStopwatch.ElapsedMilliseconds > newUserTimeS * 1000)
+        {
+            // new user
+            miniCTI.SetActive(true);
+            forceVideo = true;
+            forceVideoStopwatch.Restart();
+
+            if (currentCTIWait != null)
+            {
+                StopCoroutine(currentCTIWait);
+                currentCTIWait = null;
+            }
+        }
+
+        newUserStopwatch.Stop();
     }
 }
