eae64ba2ac752912aa00e6fa1f6674a05a4818bf
Nat Greenwood <nat.greenwood@ultrahaptics.com>
2022-04-25 15:56:41 +0100

    Add Settings Web to setup_repository script

--- Scripts/setup_repository.bat
+++ Scripts/setup_repository.bat
@@ -1,9 +1,8 @@
-@echo off
-if not "%1"=="1" (
-    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList '1'"
-    exit /b
-)
+
 
 cd %~dp0
 
+mkdir "../TF_Application/Assets/TouchFree/"
 mklink /J "../TF_Application/Assets/TouchFree/Tooling" "../TF_Settings_and_Tooling_Unity/Assets/TouchFree/Tooling"
+
+mklink /J "../TF_Settings_Web/src/TouchFree" "../TF_Tooling_Web/src"
