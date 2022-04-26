f64d35e7c5ca44fa30441c055339d18549a49ebf
Gabe Homewood <gabriel.homewood@ultraleap.com>
2022-04-26 17:37:12 +0100

    TF-500 - Retain some logged data and log time of service start

--- TF_Service_dotNet/TouchFree_Service/Program.cs
+++ TF_Service_dotNet/TouchFree_Service/Program.cs
@@ -20,12 +20,30 @@ namespace Ultraleap.TouchFree.Service
                 Directory.CreateDirectory(loggingFileDirectory);
             }
 
-            FileStream filestream = new FileStream(loggingFileDirectory + "log.txt", FileMode.Create);
+            var filename = loggingFileDirectory + "log.txt";
+
+            if (File.Exists(filename))
+            {
+                var fileInfo = new FileInfo(filename);
+                var fileSize = fileInfo.Length;
+                if (fileSize > 100000)
+                {
+                    File.Move(filename, filename.Replace("log.txt", "log_old.txt"), true);
+                }
+            }
+
+            FileStream filestream = new FileStream(filename, FileMode.Append);
             StreamWriter streamwriter = new StreamWriter(filestream)
             {
                 AutoFlush = true
             };
+
             Console.SetOut(streamwriter);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"{DateTime.Now} - Starting Service");
+            Console.WriteLine();
 #endif
             CreateHostBuilder(args).Build().Run();
         }
