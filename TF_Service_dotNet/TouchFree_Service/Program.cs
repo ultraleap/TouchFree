0698d15652ddbb2e53a2f4fe32993d95bbd282e4
Rodolphe Houdas <rodolphe.houdas@ultraleap.com>
2022-05-10 11:56:39 +0100

    Add interface and port settable through env
    The interface can be set by setting TF_INTERFACE.
    The port can be set by setting TF_PORT.
    Both of these values should be string.

--- TF_Service_dotNet/TouchFree_Service/Program.cs
+++ TF_Service_dotNet/TouchFree_Service/Program.cs
@@ -3,16 +3,22 @@ using Ultraleap.TouchFree.Library.Configuration;
 #endif
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Ultraleap.TouchFree.Service
 {
     public class Program
     {
+        private static string listeningInterface = "localhost";
+        private static string port = "9739";
+
         static void Main(string[] args)
         {
 #if !DEBUG
             TouchFreeLog.SetUpLogging();
 #endif
+            listeningInterface = Environment.GetEnvironmentVariable("TF_INTERFACE") ?? listeningInterface;
+            port = Environment.GetEnvironmentVariable("TF_PORT") ?? port;
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,7 +27,7 @@ namespace Ultraleap.TouchFree.Service
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://localhost:9739");
+                    webBuilder.UseUrls($"http://{listeningInterface}:{port}");
                 });
     }
 }
