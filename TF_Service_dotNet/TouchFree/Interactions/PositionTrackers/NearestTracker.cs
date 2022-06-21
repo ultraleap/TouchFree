fb561633aee60018ff6a190bf8ef623e8593606c
Gabe Homewood <gabriel.homewood@ultraleap.com>
2022-06-21 14:28:33 +0100

    Simplify and improve the nearest finger position tracker

--- TF_Service_dotNet/TouchFree/Interactions/PositionTrackers/NearestTracker.cs
+++ TF_Service_dotNet/TouchFree/Interactions/PositionTrackers/NearestTracker.cs
@@ -19,37 +19,19 @@ namespace Ultraleap.TouchFree.Library.Interactions.PositionTrackers
             Leap.Finger.FingerType fingerType = Leap.Finger.FingerType.TYPE_UNKNOWN;
             Leap.Bone.BoneType boneType = Leap.Bone.BoneType.TYPE_INVALID;
 
-            // check the last used finger hasn't changed position by a lot. If it hasn't, use it
-            // by default with a bias
-            foreach (var finger in hand.Fingers)
-            {
-                if (finger.Type == lastUsedFingerType)
-                {
-                    foreach (var bone in finger.bones)
-                    {
-                        if (bone.Type == lastUsedBoneType)
-                        {
-                            Vector3 jointPos = Utilities.LeapVectorToNumerics(bone.NextJoint);
-
-                            nearestDistance = jointPos.Z - NEAREST_BONE_BIAS; // add a bias to the previous finger tip position
-
-                            nearestJointPos = jointPos;
-                            fingerType = finger.Type;
-                            boneType = bone.Type;
-
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // Loop through all other fingers
+            // Loop through all fingers and bones
             foreach (var finger in hand.Fingers)
             {
                 foreach (var bone in finger.bones)
                 {
                     Vector3 jointPos = Utilities.LeapVectorToNumerics(bone.NextJoint);
                     float screenDistance = jointPos.Z;
+
+                    // add the bias for the previous finger bone position
+                    if (finger.Type == lastUsedFingerType && bone.Type == lastUsedBoneType)
+                    {
+                        screenDistance -= NEAREST_BONE_BIAS;
+                    }
 
                     if (nearestDistance > screenDistance)
                     {
