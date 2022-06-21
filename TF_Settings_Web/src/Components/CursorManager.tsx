5c0141bb95fbdeb27d8a8a50933d2de71e405112
Gabe Homewood <gabriel.homewood@ultraleap.com>
2022-06-21 13:51:22 +0100

    Update to fix build

--- TF_Settings_Web/src/Components/CursorManager.tsx
+++ TF_Settings_Web/src/Components/CursorManager.tsx
@@ -1,11 +1,14 @@
 import { SVGCursor } from '../TouchFree/Cursors/SvgCursor';
+import { TouchlessCursor } from '../TouchFree/Cursors/TouchlessCursor';
 
 export class CursorManager {
+    public cursor: TouchlessCursor;
+
     constructor() {
         let svgCanvas = document.getElementById('svg-cursor');
         let svgDot = document.getElementById('svg-cursor-dot');
         let svgRing = document.getElementById('svg-cursor-ring');
 
-        const svgCursor = new SVGCursor(svgCanvas, svgDot, svgRing, 'cx', 'cy');
+        this.cursor = new SVGCursor(svgCanvas, svgDot, svgRing, 'cx', 'cy');
     }
 }
