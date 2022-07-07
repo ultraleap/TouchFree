eb1162359a9b4ea2d609ca65f6473de703bfe3b2
Jordan Berni <jordan.berni@ultraleap.com>
2022-07-07 09:37:32 +0100

    Add navigator requests for video and usb

--- TF_Settings_Web/src/Components/Pages/Camera/CameraSetupScreen.tsx
+++ TF_Settings_Web/src/Components/Pages/Camera/CameraSetupScreen.tsx
@@ -1,5 +1,6 @@
 import 'Styles/Camera/Camera.css';
 
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import ManualSetupIcon from 'Images/Camera/Manual_Setup_Icon.svg';
@@ -8,6 +9,47 @@ import QuickSetupIcon from 'Images/Camera/Quick_Setup_Icon.svg';
 import IconTextButton from 'Components/Controls/IconTextButton';
 
 const CameraSetupScreen = () => {
+    const videoRef = React.useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        getVideo();
+    }, [videoRef]);
+
+    const getVideo = () => {
+        navigator.mediaDevices.enumerateDevices().then((list) => {
+            // console.log(list);
+            const device = list.find((device) => device.label.includes('Leap Motion'));
+            if (!device) {
+                console.error('Can not find device');
+                return;
+            }
+            navigator.mediaDevices
+                .getUserMedia({ video: { deviceId: device.deviceId } })
+                .then((stream) => {
+                    const video = videoRef.current;
+                    if (!video) return;
+                    video.srcObject = stream;
+                    video.play();
+                })
+                .catch((error) => {
+                    // SEE IF TRACKING SERVICE IS CLAIMING CAMERA
+                    console.error('error:', error);
+                });
+        });
+    };
+
+    const getUSB = () => {
+        navigator.usb
+            .getDevices()
+            .then((list) => {
+                console.log(list);
+            })
+            .catch((error) => {
+                // SEE IF TRACKING SERVICE IS CLAIMING CAMERA
+                console.error('error:', error);
+            });
+    };
+
     const navigate = useNavigate();
     return (
         <div>
@@ -34,6 +76,13 @@ const CameraSetupScreen = () => {
                     onClick={() => navigate('manual')}
                 />
             </div>
+            <video ref={videoRef} style={{ height: '300px', marginTop: '100px' }} />
+            <button
+                style={{ width: '500px', height: '100px', marginTop: '100px', background: 'pink' }}
+                onClick={getUSB}
+            >
+                CLICK FOR USB
+            </button>
         </div>
     );
 };
