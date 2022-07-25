13699f65bed8703a9772f9ead8a5749de4a049de
Jordan Berni <jordan.berni@ultraleap.com>
2022-07-25 13:36:30 +0100

    WebSettings: Refactor masking screen

---
+++ TF_Settings_Web/src/Components/Pages/Camera/Masking/MaskingOptions.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { ToggleSwitch } from 'Components/Controls/ToggleSwitch';
+
+interface MaskingOptionProps {
+    title: string;
+    description: string;
+    value: boolean;
+    onChange: (value: boolean) => void;
+}
+
+const MaskingOption: React.FC<MaskingOptionProps> = ({ title, description, value, onChange }) => (
+    <label className="cam-feeds-option">
+        <div className="cam-feeds-option-text">
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </div>
+        <div className="cam-feeds-option-toggle">
+            <ToggleSwitch value={value} onChange={onChange} />
+        </div>
+    </label>
+);
+
+export default MaskingOption;
