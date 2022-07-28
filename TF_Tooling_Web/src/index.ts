fd00c4e7482d197f9a9f6a49029c9a0f373f5b82
Tom Cartwright <tom.cartwright@ultraleap.com>
2022-07-28 11:07:09 +0100

    Changes to allow for npm formated packaging

--- TF_Tooling_Web/src/index.ts
+++ TF_Tooling_Web/src/index.ts
@@ -1,15 +1,8 @@
-import * as Configuration from './Configuration'
-import * as Connection from './Connection'
-import * as Cursors from './Cursors'
+import * as Configuration from './Configuration';
+import * as Connection from './Connection';
+import * as Cursors from './Cursors';
 import * as InputControllers from './InputControllers';
 import * as Plugins from './Plugins';
 import * as TouchFreeToolingTypes from './TouchFreeToolingTypes';
 
-module.exports = {
-    Configuration: Configuration,
-    Connection: Connection,
-    Cursors: Cursors,
-    InputControllers: InputControllers,
-    Plugins: Plugins,
-    TouchFreeToolingTypes: TouchFreeToolingTypes,
-}
+export { Configuration, Connection, Cursors, InputControllers, Plugins, TouchFreeToolingTypes };
--- TF_Tooling_Web/webpack.config.js
+++ TF_Tooling_Web/webpack.config.js
@@ -1,41 +1,30 @@
-const { Compiler } = require('webpack');
+const path = require('path');
 
 module.exports = {
     entry: {
-        TouchFree_Tooling: {
-            import: './src/index.ts',
-            library: {
-                name: "TouchFree",
-                type: "umd",
-            },
-            filename: 'dist/TouchFree_Tooling.js'
-        },
-        Snapping_Plugin: {
-            import: './plugins/SnappingPlugin/src/SnappingPlugin.ts',
-            dependOn: 'TouchFree_Tooling',
-            library: {
-                name: "SnappingPlugin",
-                type: "umd",
-            },
-            filename: 'Plugins/SnappingPlugin/Snapping_Plugin.js'
-        },
+        'touchfree-tooling': './src/index.ts',
+        'snapping-plugin': './Plugins/SnappingPlugin/src/SnappingPlugin.ts',
     },
     mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        library: '[name]',
+        libraryTarget: 'umd',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
     module: {
         rules: [
             {
+                test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
         ],
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
     optimization: {
         minimize: false,
     },
-    output: {
-        path: __dirname,
-    }
 };
