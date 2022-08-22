ab8402b3184a486cb19b0d3e73b3714e5d8c6c5f
Jordan Berni <jordan.berni@ultraleap.com>
2022-08-22 15:48:40 +0100

    WebSettings: Flip webgl image

--- TF_Settings_Web/src/Components/Pages/Camera/Masking/displayLensFeeds.ts
+++ TF_Settings_Web/src/Components/Pages/Camera/Masking/displayLensFeeds.ts
@@ -77,6 +77,7 @@ export const setupWebGL = (gl: WebGLRenderingContext) => {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.canvas.width, gl.canvas.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
 
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
