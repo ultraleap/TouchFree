30e5072c8617026093344f2429fbacf7da71768a
Tom Cartwright <tom.cartwright@ultraleap.com>
2022-09-05 13:53:35 +0100

    Updated cursor manager to fix CI pipeline

--- TF_Settings_Web/src/Components/CursorManager.tsx
+++ TF_Settings_Web/src/Components/CursorManager.tsx
@@ -1,10 +1,14 @@
-import { SVGCursor } from 'TouchFree/Cursors/SvgCursor';
-import { TouchlessCursor } from 'TouchFree/Cursors/TouchlessCursor';
+import { SVGCursor } from '../TouchFree/Cursors/SvgCursor';
+import { TouchlessCursor } from '../TouchFree/Cursors/TouchlessCursor';
 
 export class CursorManager {
     public cursor: TouchlessCursor;
 
+    public static instance: CursorManager;
+
     constructor() {
         this.cursor = new SVGCursor();
+
+        CursorManager.instance = this;
     }
 }
