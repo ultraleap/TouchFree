509d3cbc1a97f4a39c65a0f26f8d9e062023d930
Jordan Berni <jordan.berni@ultraleap.com>
2022-09-08 11:57:41 +0100

    Remove formatting

--- TF_Tooling_Web/src/Cursors/TouchlessCursor.ts
+++ TF_Tooling_Web/src/Cursors/TouchlessCursor.ts
@@ -1,6 +1,7 @@
-import { TouchFreeInputAction } from "../TouchFreeToolingTypes";
-import { InputActionManager } from "../Plugins/InputActionManager";
-import { SwipeDirection } from "../Connection/TouchFreeServiceTypes";
+import { TouchFreeInputAction } from '../TouchFreeToolingTypes';
+import { InputActionManager } from '../Plugins/InputActionManager';
+import { SwipeDirection } from '../Connection/TouchFreeServiceTypes';
+
 
 // Class: TouchlessCursor
 // This class is a base class for creating custom Touchless cursors for use with TouchFree Tooling.
@@ -9,67 +10,63 @@ import { SwipeDirection } from "../Connection/TouchFreeServiceTypes";
 //
 // For an example of a reactive cursor, see <DotCursor>.
 export abstract class TouchlessCursor {
-  // Group: Variables
+    // Group: Variables
 
-  // Variable: cursor
-  // The HTMLElement that represents this cursor
-  cursor: HTMLElement | SVGElement | undefined;
+    // Variable: cursor
+    // The HTMLElement that represents this cursor
+    cursor: HTMLElement | SVGElement | undefined;
 
-  // Group: Functions
+    // Group: Functions
 
-  // Function: constructor
-  // Registers the Cursor for updates from the <InputActionManager>
-  //
-  // If you intend to make use of the <WebInputController>, make sure that _cursor has the
-  // "touchfree-cursor" class. This prevents it blocking other elements from recieving events.
-  constructor(_cursor: HTMLElement | SVGElement | undefined) {
-    InputActionManager.instance.addEventListener("TransmitInputAction", ((
-      e: CustomEvent<TouchFreeInputAction>
-    ) => {
-      this.HandleInputAction(e.detail);
-    }) as EventListener);
+    // Function: constructor
+    // Registers the Cursor for updates from the <InputActionManager>
+    //
+    // If you intend to make use of the <WebInputController>, make sure that _cursor has the
+    // "touchfree-cursor" class. This prevents it blocking other elements from recieving events.
+    constructor(_cursor: HTMLElement | SVGElement | undefined) {
+        InputActionManager.instance.addEventListener('TransmitInputAction', ((e: CustomEvent<TouchFreeInputAction>) => {
+            this.HandleInputAction(e.detail);
+        }) as EventListener);
 
-    this.cursor = _cursor;
-  }
-
-  // Function: UpdateCursor
-  // Sets the position of the cursor, should be run after <HandleInputAction>.
-  UpdateCursor(_inputAction: TouchFreeInputAction): void {
-    if (this.cursor) {
-      this.cursor.style.left =
-        _inputAction.CursorPosition[0] - this.cursor.clientWidth / 2 + "px";
-      this.cursor.style.top =
-        _inputAction.CursorPosition[1] - this.cursor.clientHeight / 2 + "px";
+        this.cursor = _cursor;
     }
-  }
 
-  // Function: HandleInputAction
-  // The core of the logic for Cursors, this is invoked with each <TouchFreeInputAction> as
-  // they are recieved. Override this function to implement cursor behaviour in response.
-  //
-  // Parameters:
-  //    _inputAction - The latest input action recieved from TouchFree Service.
-  HandleInputAction(_inputAction: TouchFreeInputAction): void {
-    this.UpdateCursor(_inputAction);
-  }
-
-  // Function: ShowCursor
-  // Used to make the cursor visible
-  ShowCursor(): void {
-    if (this.cursor) {
-      this.cursor.style.opacity = "1";
+    // Function: UpdateCursor
+    // Sets the position of the cursor, should be run after <HandleInputAction>.
+    UpdateCursor(_inputAction: TouchFreeInputAction): void {
+        if (this.cursor) {
+            this.cursor.style.left = (_inputAction.CursorPosition[0] - (this.cursor.clientWidth / 2)) + "px";
+            this.cursor.style.top = (_inputAction.CursorPosition[1] - (this.cursor.clientHeight / 2)) + "px";
+        }
     }
-  }
 
-  // Function: HideCursor
-  // Used to make the cursor invisible
-  HideCursor(): void {
-    if (this.cursor) {
-      this.cursor.style.opacity = "0";
+    // Function: HandleInputAction
+    // The core of the logic for Cursors, this is invoked with each <TouchFreeInputAction> as
+    // they are recieved. Override this function to implement cursor behaviour in response.
+    //
+    // Parameters:
+    //    _inputAction - The latest input action recieved from TouchFree Service.
+    HandleInputAction(_inputAction: TouchFreeInputAction): void {
+        this.UpdateCursor(_inputAction);
     }
-  }
 
-  abstract ShowCloseToSwipe(): void;
-  abstract HideCloseToSwipe(): void;
-  abstract SetSwipeDirection(direction?: SwipeDirection): void;
+    // Function: ShowCursor
+    // Used to make the cursor visible
+    ShowCursor(): void {
+        if (this.cursor) {
+            this.cursor.style.opacity = "1";
+        }
+    }
+
+    // Function: HideCursor
+    // Used to make the cursor invisible
+    HideCursor(): void {
+        if (this.cursor) {
+            this.cursor.style.opacity = "0";
+        }
+    }
+
+    abstract ShowCloseToSwipe(): void;
+    abstract HideCloseToSwipe(): void;
+    abstract SetSwipeDirection(direction?: SwipeDirection): void;
 }
--- TF_Tooling_Web/src/Plugins/InputActionManager.ts
+++ TF_Tooling_Web/src/Plugins/InputActionManager.ts
@@ -8,76 +8,81 @@ import { InputActionPlugin } from "./InputActionPlugin";
 // via the  <TransmitInputAction> event which should be listened to by any class hoping to make
 // use of incoming <TouchFreeInputActions>.
 export class InputActionManager extends EventTarget {
-  // Event: TransmitInputAction
-  // An event for transmitting <TouchFreeInputActions> that are received via the <messageReceiver> to
-  // be listened to.
+    // Event: TransmitInputAction
+    // An event for transmitting <TouchFreeInputActions> that are received via the <messageReceiver> to
+    // be listened to.
 
-  // Event: TransmitInputActionRaw
-  // An event for immediately transmitting <TouchFreeInputActions> that are received via the
-  // <messageReceiver> to be listened to. This is transmitted before any Plugins are executed.
+    // Event: TransmitInputActionRaw
+    // An event for immediately transmitting <TouchFreeInputActions> that are received via the
+    // <messageReceiver> to be listened to. This is transmitted before any Plugins are executed.
 
-  // Variable: instance
-  // The instance of the singleton for referencing the events transmitted
-  static _instance: InputActionManager;
+    // Variable: instance
+    // The instance of the singleton for referencing the events transmitted
+    static _instance: InputActionManager;
 
-  static plugins: Array<InputActionPlugin> | null = null;
+    static plugins: Array<InputActionPlugin> | null = null;
 
-  public static get instance() {
-    if (InputActionManager._instance === undefined) {
-      InputActionManager._instance = new InputActionManager();
-    }
-
-    return InputActionManager._instance;
-  }
-
-  // Function: SetPlugins
-  // Use this function to set the <InputActionPlugins> that the manager should use, as well as the order the
-  // <InputActionPlugins> should be used.
-  public static SetPlugins(_plugins: Array<InputActionPlugin>): void {
-    this.plugins = _plugins;
-  }
-
-  // Function: HandleInputAction
-  // Called by the <messageReceiver> to relay a <TouchFreeInputAction> that has been received to any
-  // listeners of <TransmitInputAction>.
-  public static HandleInputAction(_action: TouchFreeInputAction): void {
-    let rawInputActionEvent: CustomEvent<TouchFreeInputAction> =
-      new CustomEvent<TouchFreeInputAction>("TransmitInputActionRaw", {
-        detail: _action,
-      });
-    InputActionManager.instance.dispatchEvent(rawInputActionEvent);
-
-    let action = _action;
-
-    if (this.plugins !== null) {
-      for (var i = 0; i < this.plugins.length; i++) {
-        let modifiedAction = this.plugins[i].RunPlugin(action);
-
-        if (modifiedAction !== null) {
-          action = modifiedAction;
-        } else {
-          // The plugin has cancelled the InputAction entirely
-          return;
+    public static get instance() {
+        if (InputActionManager._instance === undefined) {
+            InputActionManager._instance = new InputActionManager();
         }
-      }
+
+        return InputActionManager._instance;
     }
 
-    let inputActionEvent: CustomEvent<TouchFreeInputAction> =
-      new CustomEvent<TouchFreeInputAction>("TransmitInputAction", {
-        detail: action,
-      });
+    // Function: SetPlugins
+    // Use this function to set the <InputActionPlugins> that the manager should use, as well as the order the
+    // <InputActionPlugins> should be used.
+    public static SetPlugins(_plugins: Array<InputActionPlugin>): void {
+        this.plugins = _plugins;
+    }
 
-    // Wrapping the function in a timeout of 0 seconds allows the dispatch to be asynchronous
-    setTimeout(() => {
-      InputActionManager.instance.dispatchEvent(inputActionEvent);
+    // Function: HandleInputAction
+    // Called by the <messageReceiver> to relay a <TouchFreeInputAction> that has been received to any
+    // listeners of <TransmitInputAction>.
+    public static HandleInputAction(_action: TouchFreeInputAction): void {
+
+        let rawInputActionEvent: CustomEvent<TouchFreeInputAction> = new CustomEvent<TouchFreeInputAction>(
+            'TransmitInputActionRaw',
+            { detail: _action }
+        );
+        InputActionManager.instance.dispatchEvent(rawInputActionEvent);
+
+        let action = _action;
+
+        if (this.plugins !== null) {
+            for (var i = 0; i < this.plugins.length; i++) {
+                let modifiedAction = this.plugins[i].RunPlugin(action);
+
+                if (modifiedAction !== null) {
+                    action = modifiedAction;
+                } else {
+                    // The plugin has cancelled the InputAction entirely
+                    return;
+                }
+            }
+        }
+
+        let inputActionEvent: CustomEvent<TouchFreeInputAction> = new CustomEvent<TouchFreeInputAction>(
+            'TransmitInputAction',
+            { detail: action }
+        );
+
+        // Wrapping the function in a timeout of 0 seconds allows the dispatch to be asynchronous
+        setTimeout(() => {
+            InputActionManager.instance.dispatchEvent(inputActionEvent);
+        }, 0);
+    }
+
+    public static HandleCloseToSwipe(direction?: SwipeDirection): void {
+      let closeToSwipeEvent = new CustomEvent<SwipeDirection>(
+        "TransmitCloseToSwipe",
+        { detail: direction }  
+      );
+
+      // Wrapping the function in a timeout of 0 seconds allows the dispatch to be asynchronous
+      setTimeout(() => {
+        InputActionManager.instance.dispatchEvent(closeToSwipeEvent);
     }, 0);
-  }
-
-  public static HandleCloseToSwipe(direction?: SwipeDirection): void {
-    let closeToSwipeEvent = new CustomEvent<SwipeDirection>(
-      "TransmitCloseToSwipe",
-      { detail: direction }  
-    );
-    InputActionManager.instance.dispatchEvent(closeToSwipeEvent);
-  }
+    }
 }
