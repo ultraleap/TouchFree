ceb74a1897ce3b8ddd99694fb2bb4e58f5122382
Nat Greenwood <nat.greenwood@ultrahaptics.com>
2022-11-02 17:32:50 +0000

    Use new Init in Quick-Start example, & minor change to ensure the cursor correctly initialises

--- TF_Tooling_Web/src/TouchFree.ts
+++ TF_Tooling_Web/src/TouchFree.ts
@@ -4,21 +4,26 @@ import { TouchlessCursor } from "./Cursors/TouchlessCursor";
 import { WebInputController } from "./InputControllers/WebInputController";
 
 var InputController: WebInputController;
-export var CurrentCursor : TouchlessCursor;
+export var CurrentCursor: TouchlessCursor;
 
 export function Init(_cursor: TouchlessCursor): void;
 export function Init(_svgParams?: SVGCursorParams): void;
 export function Init(_cursorOrParams?: SVGCursorParams | TouchlessCursor): void {
     ConnectionManager.init();
 
-    InputController = new WebInputController();
+    ConnectionManager.AddConnectionListener(() => {
+        InputController = new WebInputController();
 
-    if (_cursorOrParams instanceof TouchlessCursor) {
-        CurrentCursor = _cursorOrParams;
-    }
-    else if (_cursorOrParams !== undefined) {
-        CurrentCursor = SVGCursor.FromParamObj(_cursorOrParams);
-    } else {
-        CurrentCursor = new SVGCursor();
-    }
+        if (_cursorOrParams instanceof TouchlessCursor) {
+            console.log("arg was a Cursor");
+            CurrentCursor = _cursorOrParams;
+        }
+        else if (_cursorOrParams !== undefined) {
+            console.log("arg was cursor params");
+            CurrentCursor = SVGCursor.FromParamObj(_cursorOrParams);
+        } else {
+            console.log("arg was undef");
+            CurrentCursor = new SVGCursor();
+        }
+    });
 }
