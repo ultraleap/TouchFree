442e8d04104093891a584b5395e220fd7a660ea1
GabeHomewood <gabriel.homewood@ultraleap.com>
2023-01-19 12:21:06 +0000

    Add example tooling test request with handshake

--- Testing/ServiceSocketTests/features/ProofOfConcept.feature
+++ Testing/ServiceSocketTests/features/ProofOfConcept.feature
@@ -2,3 +2,8 @@ Feature: Proof the tests are working
 
     Scenario: Prove a connection to the server was made
         Given the Service is running
+
+    Scenario: Can complete handshake
+        Given the Service is connected
+        When a handshake message is sent
+        Then a handshake response is received
--- Testing/ServiceSocketTests/step_definitions/Generic.ts
+++ Testing/ServiceSocketTests/step_definitions/Generic.ts
@@ -1,4 +1,4 @@
-import { Given, When, Then } from '@cucumber/cucumber';
+import { Given, When, Then, AfterAll } from '@cucumber/cucumber';
 import { expect } from 'chai';
 import { callbackify } from 'util';
 
@@ -15,4 +15,93 @@ Given('the Service is running', function(callback) {
 
         callback();
     });
-})
+});
+
+let connectedWebSocket: WebSocket | undefined = undefined;
+
+Given('the Service is connected', function(callback) {
+    // attempt to connect via WS.
+    // if connection unsuccessful, fail
+
+    connectedWebSocket = new WebSocket("ws://127.0.0.1:9739/connect");
+
+    if (connectedWebSocket) {
+        connectedWebSocket.addEventListener('open', function () {
+            callback();
+        });
+    }
+});
+
+let responses: MessageEvent[] = [];
+
+When('a handshake message is sent',  (callback) => {
+    const handshakeMessage = {
+        action: 'VERSION_HANDSHAKE',
+        content: {
+            requestID: '6423d82e-3266-4830-82d8-c46cc17fc645',
+            TfApiVersion: '1.3.0'
+        },
+    };
+
+    if (connectedWebSocket) {
+        connectedWebSocket.addEventListener('message', (_message: MessageEvent) => {
+            responses.push(_message);
+        });
+
+        connectedWebSocket.send(JSON.stringify(handshakeMessage));
+    }
+
+    callback();
+});
+
+Then('a handshake response is received', (callback) => {
+    let checkTime = 0;
+    const intervalId = setInterval(() => {
+        checkTime += 100;
+
+        if (responses.length > 0) {
+            const response = responses[0];
+            const content = JSON.parse(response.data);
+
+            clearInterval(intervalId);
+
+            const expectedResponse = { 
+                action: 'VERSION_HANDSHAKE_RESPONSE',
+                content: {
+                  requestID: '6423d82e-3266-4830-82d8-c46cc17fc645',
+                  status: 'Success',
+                  message: 'Handshake Successful.',
+                  originalRequest: '{"requestID":"6423d82e-3266-4830-82d8-c46cc17fc645","TfApiVersion":"1.3.0"}',
+                  touchFreeVersion: '',
+                  apiVersion: '1.3.0'
+                }
+            };
+
+            if (content.action === expectedResponse.action &&
+                content.content.requestID === expectedResponse.content.requestID &&
+                content.content.status === expectedResponse.content.status &&
+                content.content.message === expectedResponse.content.message) {
+                callback();
+            } else {
+                console.log('');
+                console.log('Received:');
+                console.log(content);
+                console.log('');
+                console.log('Expected:');
+                console.log(expectedResponse);
+                throw Error('Handshake message does not match expected');
+            }
+        }
+
+        if (checkTime > 3000) {
+            clearInterval(intervalId);
+            throw Error('No message received');
+        }
+    }, 100);
+});
+
+AfterAll(() => {
+    if (connectedWebSocket && connectedWebSocket.readyState === connectedWebSocket.OPEN) {
+        connectedWebSocket.close();
+    }
+});
