e474148acf0f02bd66ebe4020846190a11bb741b
Jordan Berni <jordan.berni@ultraleap.com>
2023-01-27 11:30:48 +0000

    Begin visuals layout

--- TF_Settings_Web/src/Pages/Visuals/VisualsScreen.tsx
+++ TF_Settings_Web/src/Pages/Visuals/VisualsScreen.tsx
@@ -5,7 +5,11 @@ import React from 'react';
 
 import { useIsLinux } from '@/customHooks';
 
+import { OutlinedTextButton, RadioGroup } from '@/Components';
+
 const classes = classNames.bind(styles);
+
+const StyleOptions: string[] = ['Recommended (Light)', 'Recommended (Dark)', 'Solid (Light)', 'Solid (Dark)', 'Custom'];
 
 const VisualsScreen: React.FC = () => {
     if (useIsLinux()) return <></>;
@@ -14,8 +18,22 @@ const VisualsScreen: React.FC = () => {
         <div className={classes('container')}>
             <div className={classes('title-line')}>
                 <h1> Visuals </h1>
+                <div className={classes('misc-button-container')}>
+                    <OutlinedTextButton title="Reset to Default" onClick={() => console.log('RESET')} />
+                </div>
             </div>
-
+            <div className={classes('section-container')}>
+                <div className={classes('content')}>
+                    <div className={classes('horizontalContainer')}>
+                        <RadioGroup
+                            name="InteractionType"
+                            selected={0}
+                            options={StyleOptions}
+                            onChange={(value) => console.log(value)}
+                        />
+                    </div>
+                </div>
+            </div>
             <div className={classes('page-divider')} />
         </div>
     );
