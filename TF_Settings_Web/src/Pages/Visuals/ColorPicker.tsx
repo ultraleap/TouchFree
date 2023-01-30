44fb6f041ba76480325b9f049eda497503744629
Jordan Berni <jordan.berni@ultraleap.com>
2023-01-30 13:47:23 +0000

    Simplify tabs

--- TF_Settings_Web/src/Pages/Visuals/ColorPicker.tsx
+++ TF_Settings_Web/src/Pages/Visuals/ColorPicker.tsx
@@ -7,37 +7,24 @@ import { TabSelector } from '@/Components/Header';
 
 const classes = classNames.bind(styles);
 
+const tabNames = ['Outer Fill', 'Outer Border', 'Center Fill', 'Center Border'] as const;
+
 const ColorPicker: React.FC = () => {
     const [activeTabIndex, setActiveTabIndex] = useState<number>(0);
 
     return (
         <div className={classes('color-picker')}>
             <div className={classes('color-picker__tabs')}>
-                <TabSelector
-                    name="Outer Fill"
-                    tabIndex={0}
-                    activeTabIndex={activeTabIndex}
-                    setAsActiveTab={setActiveTabIndex}
-                />
-                <TabSelector
-                    name="Outer Border"
-                    tabIndex={1}
-                    activeTabIndex={activeTabIndex}
-                    setAsActiveTab={setActiveTabIndex}
-                />
-                <TabSelector
-                    name="Center Fill"
-                    tabIndex={2}
-                    activeTabIndex={activeTabIndex}
-                    setAsActiveTab={setActiveTabIndex}
-                />
-                <TabSelector
-                    name="Center Border"
-                    tabIndex={3}
-                    forceHideDivider
-                    activeTabIndex={activeTabIndex}
-                    setAsActiveTab={setActiveTabIndex}
-                />
+                {tabNames.map((name, index) => (
+                    <TabSelector
+                        name={name}
+                        key={index}
+                        tabIndex={index}
+                        forceHideDivider={index === tabNames.length - 1}
+                        activeTabIndex={activeTabIndex}
+                        setAsActiveTab={setActiveTabIndex}
+                    />
+                ))}
             </div>
             <div className={classes('color-picker__body')}></div>
         </div>
