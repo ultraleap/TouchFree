4a09a648471d95c279a39a48f09e4da64eb6f3c7
Jordan Berni <jordan.berni@ultraleap.com>
2023-02-10 09:20:19 +0000

    Add comment to isDesktop

--- TF_Settings_Web/src/TauriUtils.ts
+++ TF_Settings_Web/src/TauriUtils.ts
@@ -1,6 +1,8 @@
 import { open } from '@tauri-apps/api/shell';
 import { appWindow } from '@tauri-apps/api/window';
 
+// If we are running in a tauri environment (i.e. are a desktop app) then the
+// window will have the __TAURI_METADATA__ property
 export const isDesktop = () => '__TAURI_METADATA__' in window;
 
 export const toggleFullScreen = async () => {
