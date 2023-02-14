553dd2690e4b33feceefc48b2c8c616d5eee1f7b
GabeHomewood <gabriel.homewood@ultraleap.com>
2023-02-14 09:35:57 +0000

    Update tests to better run in parallel

--- Testing/ServiceSocketTests/step_definitions/Generic.ts
+++ Testing/ServiceSocketTests/step_definitions/Generic.ts
@@ -1,88 +1,65 @@
-import { Given, When, Then, After } from '@cucumber/cucumber';
+import { Given, When, Then, After } from "@cucumber/cucumber";
+import { callbackOnConfigurationErrorResonse, callbackOnConfigurationFileState, callbackOnConfigurationState, callbackOnHandshake, callbackOnServiceStatus, callbackOnTrackingServiceStatus, openWebSocketAndPerformAction, reset, sendHandshake, sendMessage } from './connectionAndSendingMethods';
 
-const WebSocket = require('ws');
+After(function() {
+    const world = this;
+    reset(world);
+});
 
-let connectedWebSocket: WebSocket | undefined = undefined;
-const responses: MessageEvent[] = [];
-let responsesSetUp = false;
-
-Given('the Service is running', (callback) => {
-    openWebSocketAndPerformAction(() => {
-        if (connectedWebSocket) {
-            connectedWebSocket.close();
-        }
+Given('the Service is running', function(callback) {
+    const world = this;
+    openWebSocketAndPerformAction(world, () => {
         callback();
     });
 });
 
-Given('the Service is connected', (callback) => {
-    openWebSocketAndPerformAction(() => {
+Given('the Service is connected', function(callback) {
+    const world = this;
+    openWebSocketAndPerformAction(world, () => {
         callback();
     });
 });
 
-When('a handshake message is sent',  () => {
-    sendHandshake();
+When('a handshake message is sent',  function() {
+    const world = this;
+    sendHandshake(world);
 });
 
-Then('a handshake response is received', (callback) => {
-    callbackOnHandshake(callback);
+Then('a handshake response is received', function(callback) {
+    const world = this;
+    callbackOnHandshake(world, callback);
 });
 
-Given('the Service is connected with handshake', (callback) => {
-    openWebSocketAndPerformAction(() => {
-        sendHandshake();
+Given('the Service is connected with handshake', function(callback) {
+    const world = this;
+    openWebSocketAndPerformAction(world, () => {
+        sendHandshake(world);
+        callbackOnHandshake(world, callback);
     });
-
-    callbackOnHandshake(callback);
 });
 
-When('service status is requested',  () => {
+When('service status is requested',  function(callback) {
+    const world = this;
     const serviceStatusMessage = {
         action: 'REQUEST_SERVICE_STATUS',
         content: {
-            requestID: '6423d82e-3266-4830-82d8-c46cc17fc646'
+            requestID: ''
         },
     };
 
-    sendMessage(serviceStatusMessage);
+    setTimeout(() => {
+        sendMessage(world, serviceStatusMessage, true);
+        callback();
+    }, 300);
 });
 
-Then('a service status response is received', (callback) => {
-    callbackOnServiceStatus(callback);
+Then('a service status response is received', function(callback) {
+    const world = this;
+    callbackOnServiceStatus(world, callback);
 });
 
-When('configuration is requested',  () => {
-    const configurationRequestMessage = {
-        action: 'REQUEST_CONFIGURATION_STATE',
-        content: {
-            requestID: '6423d82e-3266-4830-82d8-c46cc17fc647'
-        },
-    };
-
-    sendMessage(configurationRequestMessage);
-});
-
-Then('a configuration response is received', (callback) => {
-    callbackOnConfigurationState(callback);
-});
-
-When('file configuration is requested',  () => {
-    const configurationRequestMessage = {
-        action: 'REQUEST_CONFIGURATION_FILE',
-        content: {
-            requestID: '6423d82e-3266-4830-82d8-c46cc17fc648'
-        },
-    };
-
-    sendMessage(configurationRequestMessage);
-});
-
-Then('a configuration file response is received', (callback) => {
-    callbackOnConfigurationFileState(callback);
-});
-
-When('configuration is requested without a requestID',  () => {
+When('configuration is requested',  function() {
+    const world = this;
     const configurationRequestMessage = {
         action: 'REQUEST_CONFIGURATION_STATE',
         content: {
@@ -90,218 +67,61 @@ When('configuration is requested without a requestID',  () => {
         },
     };
 
-    sendMessage(configurationRequestMessage);
+    sendMessage(world, configurationRequestMessage, true);
 });
 
-Then('a configuration error response is received', (callback) => {
-    callbackOnConfigurationErrorResonse(callback);
+Then('a configuration response is received', function(callback) {
+    const world = this;
+    callbackOnConfigurationState(world, callback);
 });
 
-When('tracking service status is requested',  () => {
+When('file configuration is requested',  function() {
+    const world = this;
+    const configurationRequestMessage = {
+        action: 'REQUEST_CONFIGURATION_FILE',
+        content: {
+            requestID: ''
+        },
+    };
+
+    sendMessage(world, configurationRequestMessage, true);
+});
+
+Then('a configuration file response is received', function(callback) {
+    const world = this;
+    callbackOnConfigurationFileState(world, callback);
+});
+
+When('configuration is requested without a requestID',  function() {
+    const world = this;
+    const configurationRequestMessage = {
+        action: 'REQUEST_CONFIGURATION_STATE',
+        content: {
+            requestID: ''
+        },
+    };
+
+    sendMessage(world, configurationRequestMessage, false);
+});
+
+Then('a configuration error response is received', function(callback) {
+    const world = this;
+    callbackOnConfigurationErrorResonse(world, callback);
+});
+
+When('tracking service status is requested',  function() {
+    const world = this;
     const serviceStatusMessage = {
         action: 'GET_TRACKING_STATE',
         content: {
-            requestID: '6423d82e-3266-4830-82d8-c46cc17fc649'
+            requestID: ''
         },
     };
 
-    sendMessage(serviceStatusMessage);
+    sendMessage(world, serviceStatusMessage, true);
 });
 
-Then('a tracking service status response is received', (callback) => {
-    callbackOnTrackingServiceStatus(callback);
+Then('a tracking service status response is received', function(callback) {
+    const world = this;
+    callbackOnTrackingServiceStatus(world, callback);
 });
-
-After(() => {
-    if (connectedWebSocket && connectedWebSocket.readyState === connectedWebSocket.OPEN) {
-        connectedWebSocket.close();
-    }
-    responsesSetUp = false;
-});
-
-const openWebSocketAndPerformAction = (callback: () => void) => {
-    // attempt to connect via WS.
-    // if connection unsuccessful, fail
-
-    connectedWebSocket = new WebSocket("ws://127.0.0.1:9739/connect");
-
-    if (connectedWebSocket) {
-        connectedWebSocket.addEventListener('open', () => {
-            callback();
-        });
-    }
-}
-
-const sendMessage = (message: unknown) => {
-    if (connectedWebSocket) {
-        if (!responsesSetUp) {
-            connectedWebSocket.addEventListener('message', (_message: MessageEvent) => {
-                responses.push(_message);
-            });
-            responsesSetUp = true;
-        }
-
-        connectedWebSocket.send(JSON.stringify(message));
-    }
-}
-
-const sendHandshake = () => {
-    const handshakeMessage = {
-        action: 'VERSION_HANDSHAKE',
-        content: {
-            requestID: '6423d82e-3266-4830-82d8-c46cc17fc645',
-            TfApiVersion: '1.3.0'
-        },
-    };
-
-    sendMessage(handshakeMessage);
-};
-
-const callbackOnMessage = (callback: () => void, validation: (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => void) => {
-    let checkTime = 0;
-    const interval = 10;
-
-    const intervalId: NodeJS.Timer = setInterval(() => {
-        checkTime += interval;
-
-        const response = responses.shift();
-        if (response && typeof response.data === 'string') {
-            validation(response.data, intervalId, callback);
-        }
-
-        if (checkTime > 3000) {
-            clearInterval(intervalId);
-            throw Error('No message received');
-        }
-    }, interval);
-};
-
-const callbackOnHandshake = (callback: () => void) => {
-    callbackOnMessage(callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
-        const expectedResponse = { 
-            action: 'VERSION_HANDSHAKE_RESPONSE',
-            content: {
-                requestID: '6423d82e-3266-4830-82d8-c46cc17fc645',
-                status: 'Success',
-                message: 'Handshake Successful.',
-                originalRequest: '{"requestID":"6423d82e-3266-4830-82d8-c46cc17fc645","TfApiVersion":"1.3.0"}',
-                touchFreeVersion: '',
-                apiVersion: '1.3.0'
-            }
-        };
-
-        checkActionResponse(responseData, expectedResponse, intervalId, (received: any) => {
-            return received.content.status === expectedResponse.content.status &&
-                received.content.message === expectedResponse.content.message;
-        }, callback, 'Handshake message does not match expected');
-    });
-};
-
-const callbackOnServiceStatus = (callback: () => void) => {
-    callbackOnMessage(callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
-        const expectedResponse = { 
-            action: 'SERVICE_STATUS',
-            content: {
-                requestID: '6423d82e-3266-4830-82d8-c46cc17fc646'
-            }
-        };
-
-        checkActionResponse(responseData, expectedResponse, intervalId, () => {
-            return true;
-        }, callback, 'Service Status message does not match expected');
-    });
-};
-
-const callbackOnConfigurationState = (callback: () => void) => {
-    callbackOnMessage(callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
-        const expectedResponse = { 
-            action: 'CONFIGURATION_STATE',
-            content: {
-                requestID: '6423d82e-3266-4830-82d8-c46cc17fc647'
-            }
-        };
-
-        checkActionResponse(responseData, expectedResponse, intervalId, () => {
-            return true;
-        }, callback, 'Configuration State message does not match expected');
-    });
-};
-
-const callbackOnConfigurationFileState = (callback: () => void) => {
-    callbackOnMessage(callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
-        const expectedResponse = { 
-            action: 'CONFIGURATION_FILE_STATE',
-            content: {
-                requestID: '6423d82e-3266-4830-82d8-c46cc17fc648'
-            }
-        };
-
-        checkActionResponse(responseData, expectedResponse, intervalId, () => {
-            return true;
-        }, callback, 'Configuration File State message does not match expected');
-    });
-};
-
-const callbackOnConfigurationErrorResonse = (callback: () => void) => {
-    callbackOnMessage(callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
-        const expectedResponse = { 
-            action: 'CONFIGURATION_RESPONSE',
-            content: {
-                status: 'Failure'
-            }
-        };
-
-        checkActionResponse(responseData, expectedResponse, intervalId, (received: any) => {
-            return received.content.status === expectedResponse.content.status;
-        }, callback, 'Configuration Response message does not match expected');
-    });
-};
-
-const callbackOnTrackingServiceStatus = (callback: () => void) => {
-    callbackOnMessage(callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
-        const expectedResponse = { 
-            action: 'TRACKING_STATE',
-            content: {
-                requestID: '6423d82e-3266-4830-82d8-c46cc17fc649'
-            }
-        };
-
-        checkActionResponse(responseData, expectedResponse, intervalId, (received: any) => {
-            return received.content.mask && 
-                received.content.allowImages && 
-                received.content.cameraReversed && 
-                received.content.analyticsEnabled;
-        }, callback, 'Tracking Service Status message does not match expected');
-    });
-};
-
-const checkActionResponse = (
-    responseData: string,
-    expectedResponse: any,
-    intervalId: NodeJS.Timer,
-    validation: (received: any) => boolean,
-    callback: () => void,
-    errorMessage: string) => {
-
-    const content = JSON.parse(responseData);
-
-    if (content.action === expectedResponse.action && content.requestID === expectedResponse.requestID)  {
-        clearInterval(intervalId);
-
-        if (validation(content)) {
-            callback();
-        } else {
-            logMessageComparisonAndThrow(content, expectedResponse, errorMessage);
-        }
-    }
-};
-
-const logMessageComparisonAndThrow = (received: any, expected: any, errorMessage: string) => {
-    console.log('');
-    console.log('Received:');
-    console.log(received);
-    console.log('');
-    console.log('Expected:');
-    console.log(expected);
-    throw Error(errorMessage);
-};
---
+++ Testing/ServiceSocketTests/step_definitions/connectionAndSendingMethods.ts
@@ -0,0 +1,213 @@
+import { IWorld } from '@cucumber/cucumber';
+import { v4 as uuidgen } from 'uuid';
+
+const WebSocket = require('ws');
+
+export const reset = (world: IWorld) => {
+    if (world?.connectedWebSocket?.readyState === WebSocket.OPEN) {
+        world.connectedWebSocket.close();
+    }
+}
+
+export const openWebSocketAndPerformAction = (world: IWorld, callback: () => void) => {
+    // attempt to connect via WS.
+    // if connection unsuccessful, fail
+
+    world.connectedWebSocket = new WebSocket("ws://127.0.0.1:9739/connect");
+
+    if (world.connectedWebSocket) {
+        world.connectedWebSocket.addEventListener('open', () => {
+            callback();
+        });
+    }
+}
+
+export const sendMessage = (world: IWorld, message: any, addRequestID: boolean) => {
+    if (world?.connectedWebSocket) {
+        if (!world?.responsesSetUp) {
+            world.responses = [];
+            world.connectedWebSocket.addEventListener('message', (_message: MessageEvent) => {
+                world?.responses.push(_message);
+            });
+            world.responsesSetUp = true;
+        }
+
+        world.requestIDSet = addRequestID && message?.content
+
+        if (world.requestIDSet) {
+            world.configuredData = {
+                requestID: uuidgen().toString()
+            };
+            message.content.requestID = world.configuredData.requestID;
+        } else {
+            world.configuredData = {
+                requestID: ''
+            };
+        }
+
+        world.connectedWebSocket.send(JSON.stringify(message));
+    }
+}
+
+export const sendHandshake = (world: IWorld) => {
+    const handshakeMessage = {
+        action: 'VERSION_HANDSHAKE',
+        content: {
+            requestID: '',
+            TfApiVersion: '1.3.0'
+        },
+    };
+
+    sendMessage(world, handshakeMessage, true);
+};
+
+export const callbackOnMessage = (world: IWorld, callback: () => void, validation: (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => void, expectedResponse: any) => {
+    let checkTime = 0;
+    const interval = 10;
+
+    const intervalId: NodeJS.Timer = setInterval(() => {
+        checkTime += interval;
+
+        const response = world.responses.shift();
+        if (response && typeof response.data === 'string') {
+            validation(response.data, intervalId, callback);
+        }
+
+        if (checkTime > 4000) {
+            clearInterval(intervalId);
+            throw Error('No message received');
+        }
+    }, interval);
+};
+
+export const callbackOnHandshake = (world: IWorld, callback: () => void) => {
+    const expectedResponse = { 
+        action: 'VERSION_HANDSHAKE_RESPONSE',
+        content: {
+            requestID: world?.configuredData?.requestID,
+            status: 'Success',
+            message: 'Handshake Successful.',
+            originalRequest: '{"requestID":"6423d82e-3266-4830-82d8-c46cc17fc645","TfApiVersion":"1.3.0"}',
+            touchFreeVersion: '',
+            apiVersion: '1.3.0'
+        }
+    };
+
+    callbackOnMessage(world, callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
+        checkActionResponse(world, responseData, expectedResponse, intervalId, (received: any) => {
+            return received.content.status === expectedResponse.content.status &&
+                received.content.message === expectedResponse.content.message;
+        }, callback, 'Handshake message does not match expected');
+    }, expectedResponse);
+};
+
+export const callbackOnServiceStatus = (world: IWorld, callback: () => void) => {
+    const expectedResponse = { 
+        action: 'SERVICE_STATUS',
+        content: {
+            requestID: world?.configuredData?.requestID
+        }
+    };
+
+    callbackOnMessage(world, callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
+        checkActionResponse(world, responseData, expectedResponse, intervalId, () => {
+            return true;
+        }, callback, 'Service Status message does not match expected');
+    }, expectedResponse);
+};
+
+export const callbackOnConfigurationState = (world: IWorld, callback: () => void) => {
+    const expectedResponse = { 
+        action: 'CONFIGURATION_STATE',
+        content: {
+            requestID: world?.configuredData?.requestID
+        }
+    };
+
+    callbackOnMessage(world, callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
+        checkActionResponse(world, responseData, expectedResponse, intervalId, () => {
+            return true;
+        }, callback, 'Configuration State message does not match expected');
+    }, expectedResponse);
+};
+
+export const callbackOnConfigurationFileState = (world: IWorld, callback: () => void) => {
+    const expectedResponse = { 
+        action: 'CONFIGURATION_FILE_STATE',
+        content: {
+            requestID: world?.configuredData?.requestID
+        }
+    };
+
+    callbackOnMessage(world, callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
+        checkActionResponse(world, responseData, expectedResponse, intervalId, () => {
+            return true;
+        }, callback, 'Configuration File State message does not match expected');
+    }, expectedResponse);
+};
+
+export const callbackOnConfigurationErrorResonse = (world: IWorld, callback: () => void) => {
+    const expectedResponse = { 
+        action: 'CONFIGURATION_RESPONSE',
+        content: {
+            status: 'Failure'
+        }
+    };
+
+    callbackOnMessage(world, callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
+        checkActionResponse(world, responseData, expectedResponse, intervalId, (received: any) => {
+            return received.content.status === expectedResponse.content.status;
+        }, callback, 'Configuration Response message does not match expected');
+    }, expectedResponse);
+};
+
+export const callbackOnTrackingServiceStatus = (world: IWorld, callback: () => void) => {
+    const expectedResponse = { 
+        action: 'TRACKING_STATE',
+        content: {
+            requestID: world?.configuredData?.requestID
+        }
+    };
+
+    callbackOnMessage(world, callback, (responseData: string, intervalId: NodeJS.Timer, callback: () => void) => {
+        checkActionResponse(world, responseData, expectedResponse, intervalId, (received: any) => {
+            return received.content.mask && 
+                received.content.allowImages && 
+                received.content.cameraReversed && 
+                received.content.analyticsEnabled;
+        }, callback, 'Tracking Service Status message does not match expected');
+    }, expectedResponse);
+};
+
+const checkActionResponse = (
+    world: IWorld, 
+    responseData: string,
+    expectedResponse: any,
+    intervalId: NodeJS.Timer,
+    validation: (received: any) => boolean,
+    callback: () => void,
+    errorMessage: string) => {
+
+    const content = JSON.parse(responseData);
+
+    if (content.action === expectedResponse.action &&
+        (!world?.requestIDSet || content.content.requestID === expectedResponse.content.requestID))  {
+        clearInterval(intervalId);
+
+        if (validation(content)) {
+            callback();
+        } else {
+            logMessageComparisonAndThrow(content, expectedResponse, errorMessage);
+        }
+    }
+};
+
+const logMessageComparisonAndThrow = (received: any, expected: any, errorMessage: string) => {
+    console.log('');
+    console.log('Received:');
+    console.log(received);
+    console.log('');
+    console.log('Expected:');
+    console.log(expected);
+    throw Error(errorMessage);
+};
