aad30b6f2ebfa85225f733c8a7834d1e345d206b
matt.filer@ultraleap.com <matt.filer@ultraleap.com>
2023-03-07 13:56:43 +0000

    Read address and port from config

--- TF_Service_dotNet/TouchFree_Service/Program.cs
+++ TF_Service_dotNet/TouchFree_Service/Program.cs
@@ -1,12 +1,18 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.IO;
+using System;
 using Ultraleap.TouchFree.Library.Configuration;
+using Newtonsoft.Json.Linq;
 
 namespace Ultraleap.TouchFree.Service
 {
     public class Program
     {
+        private static string serviceIP = "localhost";
+        private static string servicePort = "9739";
+
         static void Main(string[] args)
         {
 #if !DEBUG
@@ -14,6 +20,17 @@ namespace Ultraleap.TouchFree.Service
 #endif
 
             TouchFreeLog.WriteLine($"TouchFree Version: v{VersionManager.Version}");
+            TouchFreeLog.WriteLine();
+
+            string pathToConfig = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Ultraleap\\TouchFree\\Configuration\\TrackingConfig.json");
+            if (File.Exists(pathToConfig))
+            {
+                JObject obj = JObject.Parse(File.ReadAllText(pathToConfig));
+                if (obj.ContainsKey("ServiceIP")) serviceIP = obj["ServiceIP"].ToString();
+                if (obj.ContainsKey("ServicePort")) servicePort = obj["ServicePort"].ToString();
+            }
+
+            TouchFreeLog.WriteLine("TouchFree IP: http://" + serviceIP + ":" + servicePort);
             TouchFreeLog.WriteLine();
 
             CreateHostBuilder(args).Build().Run();
@@ -24,7 +41,7 @@ namespace Ultraleap.TouchFree.Service
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://localhost:9739");
+                    webBuilder.UseUrls("http://" + serviceIP + ":" + servicePort);
 #if !DEBUG
                     webBuilder.ConfigureLogging((loggingBuilder) => loggingBuilder.SetMinimumLevel(LogLevel.Warning));
 #endif
