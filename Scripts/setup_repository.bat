ba05d8e8eb800f69635189c225bc3e1035543aa6
matt.filer@ultraleap.com <matt.filer@ultraleap.com>
2023-03-10 12:17:05 +0000

    symlink bat update

--- Scripts/setup_repository.bat
+++ Scripts/setup_repository.bat
@@ -7,4 +7,6 @@ if not "%1"=="1" (
 cd %~dp0
 
 mkdir "../TF_Application/Assets/TouchFree/"
-mklink /J "../TF_Application/Assets/TouchFree/Tooling" "../TF_Settings_and_Tooling_Unity/Assets/TouchFree/Tooling"
+mklink /J "../TF_Application/Assets/TouchFree/Tooling" "../TF_Tooling_Unity/Assets/TouchFree/Tooling"
+mkdir "../TF_Settings_Unity/Assets/TouchFree/"
+mklink /J "../TF_Settings_Unity/Assets/TouchFree/Tooling" "../TF_Tooling_Unity/Assets/TouchFree/Tooling"
