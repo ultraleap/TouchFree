7941e278d2444569975e46b672710706d60acdb5
matt.filer@ultraleap.com <matt.filer@ultraleap.com>
2023-03-10 12:38:54 +0000

    actually, include all from tf folder

--- Scripts/setup_repository.bat
+++ Scripts/setup_repository.bat
@@ -6,7 +6,5 @@ if not "%1"=="1" (
 
 cd %~dp0
 
-mkdir "../TF_Application/Assets/TouchFree/"
-mklink /J "../TF_Application/Assets/TouchFree/Tooling" "../TF_Tooling_Unity/Assets/TouchFree/Tooling"
-mkdir "../TF_Settings_Unity/Assets/TouchFree/"
-mklink /J "../TF_Settings_Unity/Assets/TouchFree/Tooling" "../TF_Tooling_Unity/Assets/TouchFree/Tooling"
+mklink /J "../TF_Application/Assets/TouchFree" "../TF_Tooling_Unity/Assets/TouchFree"
+mklink /J "../TF_Settings_Unity/Assets/TouchFree" "../TF_Tooling_Unity/Assets/TouchFree"
