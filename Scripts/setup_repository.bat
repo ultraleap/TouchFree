1c617d4e1e73ee5f525ceee8cfc99bf939e796c9
matt.filer@ultraleap.com <matt.filer@ultraleap.com>
2023-03-10 13:20:45 +0000

    move plugins around

--- Scripts/setup_repository.bat
+++ Scripts/setup_repository.bat
@@ -6,5 +6,5 @@ if not "%1"=="1" (
 
 cd %~dp0
 
-mklink /J "../TF_Application/Assets/TouchFree" "../TF_Tooling_Unity/Assets/TouchFree"
-mklink /J "../TF_Settings_Unity/Assets/TouchFree" "../TF_Tooling_Unity/Assets/TouchFree"
+mklink /J "../TF_Application/Assets/TouchFree" "../TF_Tooling_Unity/Assets"
+mklink /J "../TF_Settings_Unity/Assets/TouchFree" "../TF_Tooling_Unity/Assets"
