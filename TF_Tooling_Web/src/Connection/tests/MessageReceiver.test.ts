9b430267c3e9716c3a2ac91929b7e71dac427847
Tom Cartwright <tom.cartwright@ultraleap.com>
2023-03-24 16:32:25 +0000

    Fix first test not running

--- TF_Tooling_Web/src/Connection/tests/MessageReceiver.test.ts
+++ TF_Tooling_Web/src/Connection/tests/MessageReceiver.test.ts
@@ -40,7 +40,7 @@ describe('MessageReceiver', () => {
 
     const mockOpen = () => serviceConnection?.webSocket.dispatchEvent(new Event('open'));
 
-    afterEach(() => {
+    beforeEach(() => {
         // Reset service after each test to completely reset mocks
         TouchFree.Init();
         serviceConnection = ConnectionManager.serviceConnection();
@@ -52,8 +52,6 @@ describe('MessageReceiver', () => {
     });
 
     it('should correctly handle a handshake warning', async () => {
-        if (!serviceConnection) return;
-
         const consoleTestFn = jest.spyOn(console, 'warn');
         consoleTestFn.mockImplementation(() => {});
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -70,8 +68,6 @@ describe('MessageReceiver', () => {
     });
 
     it('should correctly check for a handshake response without a callback', async () => {
-        if (!serviceConnection) return;
-
         const consoleTestFn = jest.spyOn(console, 'warn');
         consoleTestFn.mockImplementation(() => {});
         const consoleMessage = message;
@@ -93,8 +89,6 @@ describe('MessageReceiver', () => {
     });
 
     it('should correctly check for a handshake response', async () => {
-        if (!serviceConnection) return;
-
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const testFn = jest.spyOn(serviceConnection as any, 'ConnectionResultCallback');
         testFn.mockImplementation(() => {});
@@ -108,7 +102,6 @@ describe('MessageReceiver', () => {
     });
 
     it('should correctly check for a response', async () => {
-        if (!serviceConnection) return;
         const guid = uuidgen();
 
         // Test without callback
@@ -121,7 +114,7 @@ describe('MessageReceiver', () => {
         const testFn = jest.fn();
         mockOpen();
         const guid2 = uuidgen();
-        serviceConnection.SendMessage('test', guid2, testFn);
+        serviceConnection?.SendMessage('test', guid2, testFn);
 
         onMessage(ActionCode.SERVICE_STATUS_RESPONSE, undefined, guid2);
 
@@ -142,8 +135,6 @@ describe('MessageReceiver', () => {
     });
 
     it('should correctly check for a config state', async () => {
-        if (!serviceConnection) return;
-
         // Test without callback
         const consoleTestFn = jest.spyOn(console, 'warn');
         consoleTestFn.mockImplementation(() => {});
@@ -153,7 +144,7 @@ describe('MessageReceiver', () => {
         // Test with callback
         const testFn = jest.fn();
         mockOpen();
-        serviceConnection.RequestConfigState(testFn);
+        serviceConnection?.RequestConfigState(testFn);
 
         onMessage(ActionCode.CONFIGURATION_STATE);
 
@@ -164,8 +155,6 @@ describe('MessageReceiver', () => {
     });
 
     it('should correctly check for the service status', async () => {
-        if (!serviceConnection) return;
-
         mockOpen();
 
         // Test without callback
@@ -176,7 +165,7 @@ describe('MessageReceiver', () => {
 
         // Test with callback
         const testFn = jest.fn();
-        serviceConnection.RequestServiceStatus(testFn);
+        serviceConnection?.RequestServiceStatus(testFn);
 
         onMessage(ActionCode.SERVICE_STATUS);
 
@@ -187,11 +176,9 @@ describe('MessageReceiver', () => {
     });
 
     it('should correctly check for the tracking state response', async () => {
-        if (!serviceConnection) return;
-
         const testFn = jest.fn();
         mockOpen();
-        serviceConnection.RequestTrackingState(testFn);
+        serviceConnection?.RequestTrackingState(testFn);
 
         onMessage(ActionCode.TRACKING_STATE);
 
@@ -199,8 +186,6 @@ describe('MessageReceiver', () => {
     });
 
     it('should correctly check for a hand presence event', async () => {
-        if (!serviceConnection) return;
-
         const testFn = jest.spyOn(ConnectionManager, 'HandleHandPresenceEvent');
         testFn.mockImplementation(() => {});
         mockOpen();
@@ -211,8 +196,6 @@ describe('MessageReceiver', () => {
     });
 
     it('should correctly check for an interaction zone event', async () => {
-        if (!serviceConnection) return;
-
         const testFn = jest.spyOn(ConnectionManager, 'HandleInteractionZoneEvent');
         testFn.mockImplementation(() => {});
         mockOpen();
@@ -223,10 +206,6 @@ describe('MessageReceiver', () => {
     });
 
     it('should correctly check for an input action', async () => {
-        if (!serviceConnection) return;
-
-        // const interactionZoneTest = jest.spyOn(ConnectionManager, 'HandleInteractionZoneEvent');
-        // interactionZoneTest.mockImplementation(() => {});
         const testFn = jest.fn();
         TouchFree.RegisterEventCallback('TransmitInputAction', testFn);
         mockOpen();
