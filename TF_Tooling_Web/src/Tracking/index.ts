e2419c34b48096f75674840bc0f905b5065e026e
Craig Jeffrey <craig.jeffrey@ultraleap.com>
2023-04-19 11:15:09 +0100

    Delete web tooling

--- TF_Tooling_Web/src/Tracking/index.ts
+++
@@ -1,7 +0,0 @@
-import { TrackingManager } from './TrackingManager';
-import * as TrackingTypes from './TrackingTypes';
-
-module.exports = {
-    TrackingManager: TrackingManager,
-    TrackingTypes: TrackingTypes,
-};
